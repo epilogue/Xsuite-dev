d3b9d3e953458ef0dab742f2f6fea9a89b3f888a
frhubym <frhubym@FRD000448.smc-france>
2014-11-24 13:31:14 +0100

    envoi mail sous condition fonction et holon

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -432,7 +432,7 @@ class XpriceController extends Zend_Controller_Action {
                 
         $this->view->info_user = $info_user;
         $infos_client = new Application_Model_DbTable_Clients();
-        $info_client = $infos_client->getClientnumwp($this->info_demande_xprice->numwp_client);
+        $info_client = $infos_client->getClientnumwp($this->info_demande_xprice['numwp_client']);
         echo '<pre>',var_export($info_client),'</pre>';
         $this->view->info_client = $info_client;
         $infos_demande_article_xprice = new Application_Model_DbTable_DemandeArticlexprices();
