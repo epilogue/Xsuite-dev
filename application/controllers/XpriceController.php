61f1b1ead5aa855449879863d7bf38a19c328ef6
frhubym <frhubym@FRD000448.smc-france>
2014-11-24 13:12:05 +0100

    envoi mail sous condition fonction et holon

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -422,17 +422,17 @@ class XpriceController extends Zend_Controller_Action {
          */
         $infos_demande_xprice = new Application_Model_DbTable_Xprices();
         $info_demande_xprice = $infos_demande_xprice->getNumwp($numwp);
-        echo '<pre>', var_export($info_demande_xprice), '</pre>';
+        //echo '<pre>', var_export($info_demande_xprice), '</pre>';
         $user_id = $info_demande_xprice['id_user'];
         var_dump($user_id);
 //        exit();
         $this->view->info_demande_xprice = $info_demande_xprice;
         $infos_user = new Application_Model_DbTable_Users();
-        $info_user = $infos_user->getUserDemande($info_demande_xprice['id_user']);
+        $info_user = $infos_user->getUserDemande($this->info_demande_xprice->id_user);
         echo '<pre>', var_export($info_user), '</pre>';
         $this->view->info_user = $info_user;
         $infos_client = new Application_Model_DbTable_Clients();
-        $info_client = $infos_client->getClientnumwp($info_demande_xprice['numwp_client']);
+        $info_client = $infos_client->getClientnumwp($this->info_demande_xprice->numwp_client);
         echo '<pre>',var_export($info_client),'</pre>';
         $this->view->info_client = $info_client;
         $infos_demande_article_xprice = new Application_Model_DbTable_DemandeArticlexprices();
