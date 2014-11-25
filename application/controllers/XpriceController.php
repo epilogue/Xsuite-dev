13a55d42fe75a081c5e492512b3ee993283e72a2
frhubym <frhubym@FRD000448.smc-france>
2014-11-25 16:32:11 +0100

    code client en 9 caracteres dans l'action create  impacte sur create phtml creation page phtml pour validation chef de marché

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -182,7 +182,7 @@ class XpriceController extends Zend_Controller_Action {
             $this->view->infos_client = $infos_client;
             $query1ter ="select OOHEAD.OACHL1 from EIT.MVXCDTA.OOHEAD OOHEAD where OOHEAD.OACUNO = '{$resultat[0]['OBCUNO']}'";
             $numclientwp= odbc_fetch_array(odbc_exec($this->odbc_conn2, $query1ter));
-            $this->view->numclientwp = $numclientwp;
+            $this->view->numclientwp = $numclientwp[0];
             /*
              * information concernant  le projet industry auquel appartient le client
              *    donc à partir du code movex industry on va chercher dans la base xsuite
