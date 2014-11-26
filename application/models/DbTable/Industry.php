604da583b4a4215f5825650e79a302512cc23521
frhubym <frhubym@FRD000448.smc-france>
2014-11-26 10:49:13 +0100

    code industrie et denomination industrie a aller chercher

--- application/models/DbTable/Industry.php
+++ application/models/DbTable/Industry.php
@@ -6,8 +6,8 @@ class Application_Model_DbTable_Industry extends Zend_Db_Table_Abstract
     protected $_name = 'industry';
     
 public function getMovexIndustry($industriewp) {
-        
-        $row = $this->fetchRow("code_movex_industry = '{$industriewp}'");
+        $industriewp="$industriewp";
+        $row = $this->fetchRow("code_movex_industry = '{$industriewp}' ");
        
         if(!$row){
             throw new Exception("could not find row $industriewp");
