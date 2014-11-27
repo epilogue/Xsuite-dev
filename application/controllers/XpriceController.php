cdec49a47d8c35ead57453bfed7e0268a4cd66cb
frhubym <frhubym@FRD000448.smc-france>
2014-11-27 17:17:58 +0100

    code industrie et denomination industrie a aller chercher

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -426,7 +426,7 @@ class XpriceController extends Zend_Controller_Action {
     }
     public function validatechefregionAction(){
         $user = $this->_auth->getStorage()->read();
-       
+       $this->view->user=$user;
 
         $numwp = $this->getRequest()->getParam('numwp', null);
         //var_dump($numwp);
