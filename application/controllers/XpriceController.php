6a37f2122639d5298e3b2cd1b7e8c89611a8532b
frhubym <frhubym@FRD000448.smc-france>
2014-12-01 10:54:50 +0100

    debug1industry

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -205,13 +205,12 @@ class XpriceController extends Zend_Controller_Action {
              *    donc à partir du code movex industry on va chercher dans la base xsuite
              *  le nom de l'industrie auquel le client appartient pour ensuite l'afficher dans la vue
              */
-             if(isset($industriewp['Z2MCL1']) && $industriewp['Z2MCL1'] !="" && $industriewp['Z2MCL1'] !=" "){
+             if(isset($industriewp['Z2MCL1']) && $industriewp['Z2MCL1'] !="" && $industriewp['Z2MCL1'] !=" " && $industriewp['Z2MCL1'] !="  " ){
             $industry = new Application_Model_DbTable_Industry();
             $info_industry = $industry->getMovexIndustry($industriewp['Z2MCL1']);
-             $this->view->info_industry = $info_industry;}
-             
-             
-        elseif(empty($industriewp['Z2MCL1']) || $industriewp['Z2MCL1']==" " || $industriewp['Z2MCL1']=="" ){
+             $this->view->info_industry = $info_industry;
+             }
+        else{
             $plop10 = "SCI";
             $industry = new Application_Model_DbTable_Industry();
             $info_industry = $industry->getMovexIndustry($plop10);
