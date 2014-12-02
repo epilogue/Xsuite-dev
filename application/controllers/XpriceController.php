3011c310ec699c47789346adcd13924546c63063
frhubym <frhubym@FRD000448.smc-france>
2014-12-02 11:16:41 +0100

    debug2 mail

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -179,8 +179,8 @@ class XpriceController extends Zend_Controller_Action {
              */foreach ($this->view->resultat as $itnoarticle) {
                // var_dump($itnoarticle);
                 $query3 = "select MCHEAD.KOPCDT, MCHEAD.KOCSU3, MCHEAD.KOITNO from EIT.MVXCDTA.MCHEAD MCHEAD where MCHEAD.KOITNO = '{$itnoarticle['OBITNO']}'";
-echo $query3;
-exit();
+//echo $query3;
+//exit();
 
                 $resultats3 = odbc_Exec($this->odbc_conn2, $query3);
                 $prixciffob[] = odbc_fetch_object($resultats3);
