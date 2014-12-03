dbe3805772cf03f526ab58aeacb1a58da4dbc023
frhubym <frhubym@FRD000448.smc-france>
2014-12-03 17:10:10 +0100

    debug2 mail

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -726,7 +726,7 @@ class XpriceController extends Zend_Controller_Action {
 		$agreement1 = "I000001";
 		$agreement2 = "I000002";
 		$agreement3 = "I000003";
-                  $query =" select * from EIT.MVXCDTA.MPAGRP MPAGRP where MPAGRP.AJCONO = '$mmcono' AND MPAGRP.AJSUNO = '$supplier' AND (MPAGRP.AJAGNB = '$agreement3'  OR MPAGRP.AJAGNB = '$agreement2' OR MPAGRP.AJAGNB = '$agreement1') AND MPAGRP.AJOBV2 = '{$value['code_article']}' AND MPAGRP.AJOBV1 = '$division'  ORDER BY MPAGRP.AJAGNB";
+                  $query =" select * from EIT.MVXCDTA.MPAGRP MPAGRP where MPAGRP.AJCONO = '$mmcono' AND MPAGRP.AJSUNO = '$supplier' AND (MPAGRP.AJAGNB = '$agreement3'  OR MPAGRP.AJAGNB = '$agreement2' OR MPAGRP.AJAGNB = '$agreement1') AND MPAGRP.AJOBV2 = '{$value->code_article}' AND MPAGRP.AJOBV1 = '$division'  ORDER BY MPAGRP.AJAGNB";
                                       
             $infos_prixfobfr = odbc_fetch_array(odbc_exec($this->odbc_conn2, $query));
 //            if($infos_prixfobfr =""){
