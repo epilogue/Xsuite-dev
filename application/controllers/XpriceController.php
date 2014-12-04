19e824fb6b4878949af7108d2b78a8ff678cc51b
frhubym <frhubym@FRD000448.smc-france>
2014-12-04 13:38:43 +0100

    debug2 mail

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -269,11 +269,12 @@ class XpriceController extends Zend_Controller_Action {
                     }
                     $demande_xprice = $demandes_xprice->createDemandeArticlexprice($resultarticle['OBSAPR'], $resultarticle['OBNEPR'], $resultarticle['OBORQT'], round(100-($resultarticle['OBNEPR'] * 100 / $resultarticle['OBSAPR']), 2), $infos_offres->OBRGDT, null, null, null, null, null, $trackingNumber, $resultarticle['OBITNO'], $resultarticle['OBITDS'], $numwp);
                 }
+                echo '<pre>',var_export($prixciffob),'<pre>'; exit();
                 foreach ($prixciffob as $value) {
                    
                     $insertprix = new Application_Model_DbTable_DemandeArticlexprices();
                     $inserprix = $insertprix->InserPrixFob($value->AJPUPR, $value->AJOBV2, $numwp);
-                } echo '<pre>',var_export($value),'<pre>'; exit();
+                } 
                 /*
                  * ici, envoi des mails 
                  * NE PAS TOUCHER SOUS PEINE D'EFFONDREMENT DE L'APPLI
