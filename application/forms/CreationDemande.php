d5d9d43a6aede76d3c3be1184aa7adf0097302b2
frhubym <frhubym@FRD000448.smc-france>
2014-12-12 14:00:59 +0100

    maildbd % marge

--- application/forms/CreationDemande.php
+++ application/forms/CreationDemande.php
@@ -44,7 +44,7 @@ class Application_Form_CreationDemande  extends Zend_Form {
          */
         
         $submit = new Zend_Form_Element_Submit('submit');
-        $submit->setAttrib('id_demande_article', 'submitbutton')->setLabel("Envoyer l'xprice");
+        $submit->setAttrib('id_demande_article', 'submitbutton')->setLabel("Envoyer X-price");
         
         /*
          * ajout des élements au form
