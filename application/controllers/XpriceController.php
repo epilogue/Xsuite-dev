3cd9afeb62e0eb40728cc29b07ba46e8ae954cae
frhubym <frhubym@FRD000448.smc-france>
2014-12-19 14:11:18 +0100

    css update.phtml

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1379,7 +1379,7 @@ class XpriceController extends Zend_Controller_Action {
         $histo_rep=$this->getRequest()->getParam('histo',null);
         $this->view->histo_rep=$histo_rep;
         $param = $this->getRequest();
-       // echo '<pre>',var_export($param),'<pre>'; exit();
+        echo '<pre>',var_export($param),'<pre>'; exit();
         $infos = new Application_Model_DbTable_Xprices();
         $info = $infos->getNumwp($numwp);
         $tracking_number = $info['tracking_number_demande_xprice'];
