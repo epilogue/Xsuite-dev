d98bf33b4fd834fe81d278438b9e0b7ee05528e2
frhubym <frhubym@FRD000448.smc-france>
2014-12-26 09:33:08 +0100

    css

---
+++ application/controllers/XdistribController.php
@@ -0,0 +1,33 @@
+<?php
+
+class XdistribController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $auth = Zend_Auth::getInstance();
+        $user = $auth->getIdentity();
+        if (is_null($user)) {
+            $this->_helper->redirector('index', 'login');
+        } else {
+            /* @todo commentaire franck
+             * Et donc, ici, on peut faire de l'acl de manière plus fine
+             */
+        }
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+public function createAction()
+    {
+        // action body
+    }
+    public function consultAction()
+    {
+        // action body
+    }
+}
+
