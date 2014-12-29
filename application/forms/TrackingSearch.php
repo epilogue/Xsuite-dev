b03e15344fb86d28a9d5a8873275a02512aa2849
frhubym <frhubym@FRD000448.smc-france>
2014-12-29 15:02:35 +0100

    css

--- application/forms/TrackingSearch.php
+++ application/forms/TrackingSearch.php
@@ -14,7 +14,7 @@ class Application_Form_TrackingSearch extends Zend_Form {
                 ->setDescription("exemple: 00906XXXXXX")
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
-                ->addValidator('regex', false, array('/^SP-FR-Q[a-z][0-9][0-9][0-9][0-9][0-9]/', 'messages' => array(Zend_Validate_Regex::NOT_MATCH => "numéro invalide")))
+                ->addValidator('regex', false, array('/^SP-FR-Q[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][0-9]/', 'messages' => array(Zend_Validate_Regex::NOT_MATCH => "numéro invalide")))
                 ->addValidator('NotEmpty', true, array('messages' => array(Zend_Validate_NotEmpty::IS_EMPTY => "le champ tracking_number ne peut pas être vide.")));
 
         $submit = new Zend_Form_Element_Submit('submit');
