65946b8b616ce12d50be34a913891bc551407585
frhubym <frhubym@FRD000448.smc-france>
2014-12-29 14:52:28 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1557,7 +1557,7 @@ class XpriceController extends Zend_Controller_Action {
     }
     public function trackingAction(){
          $track = $this->getRequest()->getParam('track', null);
-        $form = new Application_Form_Tracking();
+        $form = new Application_Form_TrackingSearch();
         if (!is_null($track)) {
             $form->populate(array("tracking_number_demande_xprice" => $track));
         }
