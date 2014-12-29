03af61072f1c2977a77ac569d303c04023e8d596
frhubym <frhubym@FRD000448.smc-france>
2014-12-29 15:05:36 +0100

    css

--- application/forms/TrackingSearch.php
+++ application/forms/TrackingSearch.php
@@ -8,20 +8,20 @@ class Application_Form_TrackingSearch extends Zend_Form {
         $this->setAttrib("class", "little_form");
 
 
-        $num_offre_workplace = new Zend_Form_Element_Text('tracking_number_demande_xprice');
-        $num_offre_workplace->setLabel("numéro tracking de la Xprice : ")
+        $tracking_number_demande_xprice = new Zend_Form_Element_Text('tracking_number_demande_xprice');
+        $tracking_number_demande_xprice->setLabel("numéro tracking de la Xprice : ")
                 ->setRequired(TRUE)
-                ->setDescription("exemple: 00906XXXXXX")
+                ->setDescription("exemple: SP-FR-QX00000")
                 ->addFilter('StripTags')
                 ->addFilter('StringTrim')
                 ->addValidator('regex', false, array('/^SP-FR-Q[a-zA-Z][a-zA-Z][0-9][0-9][0-9][0-9][0-9]/', 'messages' => array(Zend_Validate_Regex::NOT_MATCH => "numéro invalide")))
                 ->addValidator('NotEmpty', true, array('messages' => array(Zend_Validate_NotEmpty::IS_EMPTY => "le champ tracking_number ne peut pas être vide.")));
 
         $submit = new Zend_Form_Element_Submit('submit');
-        $submit->setAttrib('num_offre_workplace', 'submitbutton')
+        $submit->setAttrib('tracking_number_demande_xprice', 'submitbutton')
                 ->setAttrib('class','submit')
                 ->setLabel('entrer');
-        $this->addElements(array($num_offre_workplace, $submit));
+        $this->addElements(array($tracking_number_demande_xprice, $submit));
     }
 
 }
