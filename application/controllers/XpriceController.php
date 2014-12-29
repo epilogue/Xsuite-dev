deab2eda00f56441394a4100a75c6786355b2a47
frhubym <frhubym@FRD000448.smc-france>
2014-12-29 17:16:47 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1632,7 +1632,7 @@ class XpriceController extends Zend_Controller_Action {
 
         $this->view->info_user = $info_user;
         $infos_client = new Application_Model_DbTable_Clients();
-        $info_client = $infos_client->getClientnumwp($info_demande_xprice['numwp_client']);
+        $info_client = $infos_client->getClientnumwp($info_demande_xprice->numwp_client);
         $this->view->info_client = $info_client;
         $noms_industrie = new Application_Model_DbTable_Industry();
         $nom_industrie = $noms_industrie->getIndustry($info_client['id_industry']);
