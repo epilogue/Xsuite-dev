8a5f0a71784fda8f58a9b58e3e9fb719a2a33dd4
frhubym <frhubym@FRD000448.smc-france>
2014-12-30 09:08:12 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1622,7 +1622,7 @@ class XpriceController extends Zend_Controller_Action {
             $usersValidations[$key]['fonction'] = $userValidationInfos['description_fonction'];
         }
         $this->view->usersValidations = $usersValidations;
-        echo '<pre>',  var_export($userValidationInfos),'<pre>';
+        echo '<pre>',  var_export($usersValidations),'<pre>';
         /*
          * Fin du chargement des validations
          */
