43436da54d167df2ba9a3021fa188d4a481b42e5
frhubym <frhubym@FRD000448.smc-france>
2015-01-05 10:36:59 +0100

    css

--- public/js/controllers/xprice/recalculeprix_marge.js
+++ public/js/controllers/xprice/recalculeprix_marge.js
@@ -4,14 +4,13 @@ $(document).ready(function (){
         var k = idT[1];
         var ra = Number(100 - ((parseFloat($(this).val())*100)/parseFloat($('td#pwp-'+k).html()))).toFixed(2);
         $('input#ra-'+k).val(ra+'%');
-        var ma = Number(1 -(parseFloat($('td#cif').val()))/parseFloat($(this).val())).toFixed(2);
+        var ma = Number(1 -(parseFloat($('td#cif-'+k).html()))/parseFloat($(this).val())).toFixed(2);
     });
     $('input.ra').change(function (){
         var idT = $(this).attr('id').split('-');
         var k = idT[1];
         var pwp = parseFloat($('td#pwp-'+k).html());
         var ra = parseFloat($(this).val());
-        
         var pda = Number(((100-ra)*pwp)/100).toFixed(2);
         $('input#pda-'+k).val(pda);
         $(this).val(parseFloat($(this).val())+'%')
