9b3d390b8878dd8ebc64b63bc88d17f27b73c766
frhubym <frhubym@FRD000448.smc-france>
2015-01-12 16:31:20 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1336,9 +1336,9 @@ class XpriceController extends Zend_Controller_Action {
             $commentId = $this->genererValidation($datasValidation);
 //            }
             $emailVars = Zend_Registry::get('emailVars');
-  echo '<pre>',  var_export($formData),'</pre>';exit();
+  echo '<pre>',  var_export($formData),'</pre>';
             foreach ($formData as $ploptitude) {
-                $marge = array_combine($ploptitude['code_article'],(1-($ploptitude['prix_cif_demande_article']/$ploptitude['prixwplace_demande_article'])));
+                $marge = array_combine($ploptitude['code_article'],(1-($ploptitude['prix_cif']/$ploptitude['prixwplace'])));
             }
             $margemin = false;
             echo '<pre>',  var_export($marge),'</pre>';exit();
