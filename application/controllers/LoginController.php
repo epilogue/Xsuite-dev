c194b44ace04d99b9d9b246b7225cf200c32db72
frhubym <frhubym@FRD000448.smc-france>
2015-01-14 13:27:25 +0100

    css

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -7,7 +7,7 @@ class LoginController extends Zend_Controller_Action {
     }
     
     public function indexAction() {
-       echo '<pre>', var_export($_POST),'</pre>';
+       echo '<pre>', var_export($this->request->referer()),'</pre>';
         $loginForm = new Application_Form_Login();
         $request = $this->getRequest();
 
