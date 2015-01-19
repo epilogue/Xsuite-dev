b6c11ddf614a15b4280c48705ca21d4cb8b61b82
frhubym <frhubym@FRD000448.smc-france>
2015-01-19 16:14:05 +0100

    css

--- application/models/DbTable/Distributeurs.php
+++ application/models/DbTable/Distributeurs.php
@@ -15,12 +15,15 @@ class Application_Model_DbTable_Distributeurs extends Zend_Db_Table_Abstract {
         }
     }
 
-    public function createClient($nom_distributeur, $prenom_distributeur, $numwp_distributeur, $adresse_distributeur, $id_industry,$potentiel) {
+    public function createClient($nom_distributeur,$nom_contact_distributeur, $prenom_contact_distributeur, $numwp_distributeur,$agence_distributeur, $adresse_distributeur,$id_holon, $id_industry,$potentiel) {
         $data = array(
             'nom_distributeur' => $nom_distributeur,
-            'prenom_disributeur' =>$prenom_distributeur,
+            'nom_contact_distributeur' => $nom_contact_distributeur,
+            'prenom_contact_disributeur' =>$prenom_contact_distributeur,
             'numwp_distributeur' => $numwp_distributeur,
+            'agence_distributeur' =>$agence_distributeur,
             'adresse_distributeur' => $adresse_distributeur,
+            'id_holon'=>$id_holon,
             'id_industry' => $id_industry,
             'potentiel' =>$potentiel
         );
