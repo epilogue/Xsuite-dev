8a0d04ef1a7e4a78f2d3b8f0e11def5765b1ac71
frhubym <frhubym@FRD000448.smc-france>
2015-01-22 11:02:24 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -798,7 +798,7 @@ class XpriceController extends Zend_Controller_Action {
 // echo '<pre>',  var_export($datas,true),'</pre>';exit();
             $prix_accordes = array_combine($datas['code_article'], $datas['prix_accorde_article']);
             $remise_accordes = array_combine($datas['code_article'], $datas['remise_accorde_article']);
-            $marge = array_combine($datas['code_article'],$datas['marge_demande_article']); echo '<pre>',  var_export($marge),'</pre>'; exit();
+            $marge = array_combine($datas['code_article'],$datas['marge_demande_article']); echo '<pre>',  var_export($marge),'</pre>'; 
             foreach ($remise_accordes as $key => $value) {
                 $remisesDirco = new Application_Model_DbTable_DemandeArticlexprices();
                 $remiseDirco = $remisesDirco->insertRemiseAccorde($value, $key, $datas['tracking']);
@@ -837,9 +837,9 @@ class XpriceController extends Zend_Controller_Action {
 //            }
             $margemin = false;
           
-            foreach ($marges as $key => $value2) {
+            foreach ($marge as $key => $value2) {
                 $margesmc = $value2;
-                 echo '<pre>',  var_export($margesmc),'</pre>';
+                 echo '<pre>',  var_export($margesmc),'</pre>';exit();
                 if ($margesmc < 10 || $margesmc == 0) {
                     $margemin = true;
                 }
