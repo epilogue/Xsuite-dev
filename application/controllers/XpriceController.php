a5791b48a5caba8e5eeda2284ff869781a4c1da1
epilogue <epilogue@aglae>
2015-01-30 18:41:22 +0100

    debug01

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -15,11 +15,11 @@ class XpriceController extends Zend_Controller_Action {
 
     public function init() {
         $this->dsn = Zend_Registry::get("dsnString");
-//        $this->odbc_conn = odbc_connect($this->dsn, "", "");
-        $this->odbc_conn = odbc_connect('Movex', "EU65535", "CCS65535");
-        if (!$this->odbc_conn) {
-            echo "pas d'accès à la base de données CVXDTA";
-        }
+////        $this->odbc_conn = odbc_connect($this->dsn, "", "");
+//        $this->odbc_conn = odbc_connect('Movex', "EU65535", "CCS65535");
+//        if (!$this->odbc_conn) {
+//            echo "pas d'accès à la base de données CVXDTA";
+//        }
         $this->_auth = Zend_Auth::getInstance();
         $this->view->messages = $this->_helper->flashMessenger->getMessages();
 
@@ -37,16 +37,16 @@ class XpriceController extends Zend_Controller_Action {
             $this->view->commentId = null;
         }
 
- $this->dsn2 = Zend_Registry::get("dsn2String");
-        $this->odbc_conn2 = odbc_connect('Movex2', "EU65535", "CCS65535");
-        if (!$this->odbc_conn2) {
-            echo "pas d'accès à la base de données MVXCDTA";
-    }
-        // $this->dsn3,"","");
-        $this->odbc_conn3 = odbc_connect('Movex3', "EU65535", "CCS65535");
-        if (!$this->odbc_conn3) {
-            echo "pas d'accès à la base de données SMCCDTA";
-        }
+// $this->dsn2 = Zend_Registry::get("dsn2String");
+//        $this->odbc_conn2 = odbc_connect('Movex2', "EU65535", "CCS65535");
+//        if (!$this->odbc_conn2) {
+//            echo "pas d'accès à la base de données MVXCDTA";
+//    }
+//        // $this->dsn3,"","");
+//        $this->odbc_conn3 = odbc_connect('Movex3', "EU65535", "CCS65535");
+//        if (!$this->odbc_conn3) {
+//            echo "pas d'accès à la base de données SMCCDTA";
+//        }
     }
 
     public function indexAction() {
