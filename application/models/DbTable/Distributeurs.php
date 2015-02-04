17754b30613691fafef617a82767f3bb9c5aa347
frhubym <frhubym@FRD000448.smc-france>
2015-02-04 09:48:10 +0100

    css

--- application/models/DbTable/Distributeurs.php
+++ application/models/DbTable/Distributeurs.php
@@ -19,13 +19,13 @@ class Application_Model_DbTable_Distributeurs extends Zend_Db_Table_Abstract {
         $data = array(
             'nom_distributeur' => $nom_distributeur,
             'nom_contact_distributeur' => $nom_contact_distributeur,
-            'prenom_contact_disributeur' =>$prenom_contact_distributeur,
+            'prenom_contact_distributeur' => $prenom_contact_distributeur,
             'numwp_distributeur' => $numwp_distributeur,
-            'agence_distributeur' =>$agence_distributeur,
+            'agence_distributeur' => $agence_distributeur,
             'adresse_distributeur' => $adresse_distributeur,
-            'id_holon'=>$id_holon,
+            'id_holon'=> $id_holon,
             'id_industry' => $id_industry,
-            'potentiel' =>$potentiel
+            'potentiel' => $potentiel
         );
         $this->insert($data);
         return $this;
