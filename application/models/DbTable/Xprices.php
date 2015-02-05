b7eda1553798fb5293b1d73cff3e4600bbca71c9
frhubym <frhubym@FRD000448.smc-france>
2015-02-05 14:12:27 +0100

    css

--- application/models/DbTable/Xprices.php
+++ application/models/DbTable/Xprices.php
@@ -105,7 +105,7 @@ public function searchAll($tracking_number){
     public function searchByUser($id){
         $sql="select demande_xprices.num_workplace_demande_xprices,demande_xprices.tracking_number_demande_xprice,clients.nom_client,demande_xprices.date_demande_xprice from demande_xprices "
                 . "join clients on clients.numwp_client = demande_xprices.numwp_client"
-                . "where demande_xprices.id_user = $id";
+                . " where demande_xprices.id_user = $id";
         $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchObject();
         return $rest;
