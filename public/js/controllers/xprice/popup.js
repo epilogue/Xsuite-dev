853d71f3869be0679685578f52d6717a7b9b6016
frhubym <frhubym@FRD000448.smc-france>
2015-02-05 16:39:35 +0100

    css

---
+++ public/js/controllers/xprice/popup.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ $(function() {
+$( "#dialog" ).dialog({
+autoOpen: false,
+show: {
+effect: "blind",
+duration: 1000
+},
+hide: {
+effect: "explode",
+duration: 1000
+}
+});
+$( "#historique" ).click(function() {
+     var idT = $(this).attr('id').split('-');
+        var k = idT[1];
+$( "#dialog-"+k ).dialog( "open" );
+});
+});
+
