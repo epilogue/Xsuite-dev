7311d56129abe75d139ee5db6be6c039b4cedb62
frhubym <frhubym@FRD000448.smc-france>
2015-02-10 09:55:45 +0100

    css

--- application/models/DbTable/Xprices.php
+++ application/models/DbTable/Xprices.php
@@ -119,9 +119,9 @@ public function searchAll($tracking_number){
          
         $sql="select demande_xprices.id_demande_xprice, demande_xprices.num_workplace_demande_xprice,demande_xprices.tracking_number_demande_xprice,clients.nom_client,demande_xprices.date_demande_xprice from demande_xprices "
                 . "join clients on clients.numwp_client = demande_xprices.numwp_client "
-                . "join users.id_user = demande_xprices.id_user"
+                . "join users on users.id_user = demande_xprices.id_user"
                 
-                . " where demande_xprices.tracking_number_demande_xprice like '$tracking1' or like '$tracking2' order by demande_xprices.date_demande_xprice desc";
+                . " where demande_xprices.tracking_number_demande_xprice like '$tracking1' or demande_xprices.tracking_number_demande_xprice like '$tracking2' order by demande_xprices.date_demande_xprice desc";
        
         $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
