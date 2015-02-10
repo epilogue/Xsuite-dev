b77880fc3810758b59e9b033ccd748ec5e3d9a8f
frhubym <frhubym@FRD000448.smc-france>
2015-02-10 09:27:55 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -61,17 +61,22 @@ class XpriceController extends Zend_Controller_Action {
          case 2:
              $tracking1="SP-FR-QC";
              $tracking2="SP-FR-QF";
-             echo plop1;$recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
+             $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
+             $this->view->recapitulatif = $recapitulatif2;
              break;
          case 3:
              $tracking1="SP-FR-QE";
-             $tracking2="SP-FR-QH";$recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
-             echo plop2;
+             $tracking2="SP-FR-QH";
+             $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
+             $this->view->recapitulatif = $recapitulatif2;
+             
              break;
          case 4:
             $tracking1="SP-FR-QI";
-             $tracking2="SP-FR-QK";$recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
-             echo plop3;
+             $tracking2="SP-FR-QK";
+             $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
+             $this->view->recapitulatif = $recapitulatif2;
+             
              break;
          }
          
