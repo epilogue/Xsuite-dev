ce6e9c1ad835bda3b344e81e38a3e943b09551e9
frhubym <frhubym@FRD000448.smc-france>
2015-02-10 10:09:47 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -60,21 +60,21 @@ class XpriceController extends Zend_Controller_Action {
      
      switch ($holon){
          case "2":
-             $tracking1="SP-FR-QC%";
-             $tracking2="SP-FR-QF%";
+             $tracking1="SP-FR-QC";
+             $tracking2="SP-FR-QF";
              $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
              $this->view->recapitulatif = $recapitulatif2;
              break;
          case" 3":
-             $tracking1="SP-FR-QE%";
-             $tracking2="SP-FR-QH%";
+             $tracking1="SP-FR-QE";
+             $tracking2="SP-FR-QH";
              $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
              $this->view->recapitulatif = $recapitulatif2;
              
              break;
          case "4":
-            $tracking1="SP-FR-QI%";
-             $tracking2="SP-FR-QK%";
+            $tracking1="SP-FR-QI";
+             $tracking2="SP-FR-QK";
              $recapitulatif2=$recapitulatif1->searchByCDR($tracking1, $tracking2);
              $this->view->recapitulatif = $recapitulatif2;
              
--- application/models/DbTable/Xprices.php
+++ application/models/DbTable/Xprices.php
@@ -121,7 +121,7 @@ public function searchAll($tracking_number){
                 . "join clients on clients.numwp_client = demande_xprices.numwp_client "
                 . "join users on users.id_user = demande_xprices.id_user"
                 
-                . " where demande_xprices.tracking_number_demande_xprice like '$tracking1' or demande_xprices.tracking_number_demande_xprice like '$tracking2' order by demande_xprices.date_demande_xprice desc";
+                . " where demande_xprices.tracking_number_demande_xprice like '$tracking1%' or demande_xprices.tracking_number_demande_xprice like '$tracking2%' order by demande_xprices.date_demande_xprice desc";
        
         $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
