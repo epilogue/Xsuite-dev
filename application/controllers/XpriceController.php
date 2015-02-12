313b499e6e05e42f7e3e53b546c09d0682001c5e
frhubym <frhubym@FRD000448.smc-france>
2015-02-12 14:50:43 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -2005,7 +2005,7 @@ class XpriceController extends Zend_Controller_Action {
         $fermeture = $ferme->searchFermeture($numwp);
         $this->view->fermeturevalide = $fermeture;
        echo '<pre>', var_export($fermeture),'</pre>';
-       echo '<pre>', var_export($fermeture[0]),'</pre>';
+       echo '<pre>', var_export($fermeture[0]['etat_validation']),'</pre>';
        echo '<pre>', var_export($fermeture[0][0]),'</pre>';
         /*
          * on va rechercher les informations concernant la demande _xprice
