f29b07e08469931fba2a4fbc31cd5fcb3b8c3a60
frhubym <frhubym@FRD000448.smc-france>
2015-02-12 17:09:03 +0100

    css

--- public/js/controllers/xprice/fermeturevalide.js
+++ public/js/controllers/xprice/fermeturevalide.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function (){
+
     var plop= $('input#fermeturevalide').val();
 
 function changeyourbody(){
@@ -12,4 +12,4 @@ function changeyourbody(){
     }
     
 };
-});
+
