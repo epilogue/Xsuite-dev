fca5de302fdb611414064fdb1ce9a742edd04fd2
frhubym <frhubym@FRD000448.smc-france>
2015-02-19 10:35:12 +0100

    css

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -111,7 +111,6 @@ if($user->id_fonction == 3){
          $recapitulatif2 = $r;
      }
     $this->view->recapitulatif = $recapitulatif2;
-   echo '<pre>',var_export($recapitulatif2),'</pre>';
 }
 
     public function numwpAction() {
