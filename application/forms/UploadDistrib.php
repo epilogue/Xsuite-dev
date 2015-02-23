55e67151745c3392a4e64529920f474ff01e3380
frhubym <frhubym@FRD000448.smc-france>
2015-02-23 10:37:49 +0100

    css

--- application/forms/UploadDistrib.php
+++ application/forms/UploadDistrib.php
@@ -17,6 +17,7 @@ class Application_Form_UploadDistrib extends Zend_Form {
                 ->addValidator('regex', false, array('/^00[9][0][6][0-9]/', 'messages' => array(Zend_Validate_Regex::NOT_MATCH => "numéro invalide")))
                 ->addValidator('NotEmpty', true, array('messages' => array(Zend_Validate_NotEmpty::IS_EMPTY => "le champ offre workplace ne peut pas être vide.")));
         $nomfichier = new Zend_Form_Element_File('nomfichier');
+        $nomfichier->setAttrib('class','uploadFichier');
 
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setAttrib('num_offre_workplace','nomfichier', 'submitbutton')
