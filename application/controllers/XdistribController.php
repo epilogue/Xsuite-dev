9bc4d6d70ef33b7f740ee7409c85bc2ee09d2271
frhubym <frhubym@FRD000448.smc-france>
2015-02-24 10:59:03 +0100

    css

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -470,7 +470,7 @@ $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
 
 echo '<hr />';
 
-$sheetData = $objPHPExcel->getActiveSheet()->getActiveRow();
+$sheetData = $objPHPExcel->getActiveSheet()->getRowIterator();
 //
 echo '<pre>',  var_export($sheetData),'</pre>';
 
