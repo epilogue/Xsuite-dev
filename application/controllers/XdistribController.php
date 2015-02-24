f2787bd01355febe66222afe5d3b336a09d30456
frhubym <frhubym@FRD000448.smc-france>
2015-02-24 17:06:39 +0100

    css

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -476,18 +476,6 @@ $objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
 * @var PHPExcel_Worksheet $sheet
 */
 $sheet = $objPHPExcel->getSheet(0);
-$rows=$objPHPExcel->getActiveSheet()->getHighestRow();
-for($row=0;$row<=$rows;$row++):
-    $plop1 = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-$plop2=$objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-$plop3=$objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-$plop4=$objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-$plop5=$objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-    $nom_contact = $objPHPExcel->getActiveSheet()->getCellByColumnAndRow(2,$row)->getValue();
-    echo $nom_contact;
-endfor;
-
-
 echo '<table border="1">';
  
 // On boucle sur les lignes
