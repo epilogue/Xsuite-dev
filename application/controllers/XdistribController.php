49f1c5ef7620e55141075509f083e504d3562c4f
frhubym <frhubym@FRD000448.smc-france>
2015-02-25 14:48:16 +0100

    css

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -170,9 +170,9 @@ foreach($sheet->getRowIterator() as $row) {
    echo '<pre>',var_export($client_final),'</pre>';
    echo '<pre>',var_export($distributeur),'</pre>';
   /* deuxième iteration  on va  recuperer les données  concernant les articles  on boucle tant qu'on a pas de ligne  vide  ou  que l'on ne rencontre pas ''concurrence''*/ 
-   $i=6;
+   //$i=6;
 foreach($sheet->getRowIterator() as $row) {
-    if($i<14) {
+    if($i >14) {
         $i++;
         continue;
     }
@@ -191,7 +191,7 @@ foreach($excellContent as $key=>$row){
  }   
     
 }
-//echo '<pre>',var_export($rows),'</pre>';
+echo '<pre>',var_export($rows),'</pre>';
 
 /*troisieme iteration on va chercher 
  * le nom des concurrents les references
