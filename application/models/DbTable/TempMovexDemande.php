175a6a95b59ae82ba8923d92e3a1cff61fdac472
frhubym <frhubym@FRD000448.smc-france>
2015-02-27 14:43:55 +0100

    css

--- application/models/DbTable/TempMovexDemande.php
+++ application/models/DbTable/TempMovexDemande.php
@@ -14,14 +14,17 @@
 class Application_Model_DbTable_TempMovexDemande extends Zend_Db_Table_Abstract {
     protected $_name = 'temp_movex_demande';
     
-    function createDemandeTemp($code_article,$reference_article,$prix_tarif,$prix_demande,$remise_demande,$numwp,$code_acquisition,$prix_fob,$prix_cif){
+    function createDemandeTemp($code_article,$reference_article,$prix_tarif,$quantite,$prix_demande,$remise_demande,$numwp,$prix_accorde,$remise_accorde,$code_acquisition,$prix_fob,$prix_cif){
         $data=array(
             'code_article'=>$code_article,
             'reference_article'=>$reference_article,
             'prix_tarif'=>$prix_tarif,
+            'quantite'=>$quantite,
             'prix_demande'=>$prix_demande,
             'remise_demande'=>$remise_demande,
             'numwp'=>$numwp,
+            'prix_accorde'=>$prix_accorde,
+            'remise_accorde'=>$remise_accorde,
             'code_acquisition'=>$code_acquisition,
             'prix_fob'=>$prix_fob,
             'prix_cif'=>$prix_cif);
