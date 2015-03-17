13b5b46c195e6cfa44f40810cef8040fb4dd9f72
frhubym <frhubym@FRD000448.smc-france>
2015-03-17 16:46:59 +0100

    creation sans fichier des xdistrib

---
+++ public/js/controllers/Xdistrib/calculRemiseDistrib.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function (){
+     $('input.rad').change(function (){
+         /*calcul du prix en fonction de la remise
+          *prixachat actuel= prixtarif-((prixtarif*remise)/100)
+          * 
+          */
+         var idT = $(this).attr('id').split('-');
+         var k = idT[1];
+         var pwp = parseFloat($('td#pwp-'+k).html());
+         var pad =(pwp-(( pwp*parseFloat($(this).val()))/100));
+          $('input#pad-'+k).val(pad); 
+     });
+});
