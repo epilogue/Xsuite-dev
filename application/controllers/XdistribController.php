4b4166f3bf378f12fdadd559513a853233b1ac07
frhubym <frhubym@FRD000448.smc-france>
2015-03-17 09:46:56 +0100

    creation sans fichier des xdistrib

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -89,7 +89,8 @@ class XdistribController extends Zend_Controller_Action
             $sql = "select * from EIT.CVXCDTA.OOLINE OOLINE where OOLINE.OBORNO='{$numwp}'";
             $infos_offre = odbc_exec($this->odbc_conn, $sql);
             $infos_offres = odbc_fetch_object($infos_offre);
-            echo '<pre>', var_export($infos_offres),'</pre>';
+            foreach($infos_offres as $info_offre){
+            echo '<pre>', var_export($info_offre),'</pre>';}
             $this->view->infos_offres=$infos_offres;
             /*
              *'OBDLSP-> numéro client final (10 chiffres)'
