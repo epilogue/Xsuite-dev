aad0c9dfe16ddb0debdaa7a699d822cba7bc445f
frhubym <frhubym@FRD000448.smc-france>
2015-03-18 11:17:15 +0100

    creation sans fichier des xdistrib

--- public/js/controllers/Xdistrib/calculRemiseDistrib.js
+++ public/js/controllers/Xdistrib/calculRemiseDistrib.js
@@ -21,4 +21,20 @@ $(document).ready(function (){
           $('input#pad-'+k).val(pad); 
           $(this).val(parseFloat($(this).val())+'%');
      });
+     $('input.pad').change(function(){
+          /*calcul du prix en fonction de la remise
+          *remise= 100-((pad*100)/pwp)
+          * 
+          */
+         var idT = $(this).attr('id').split('-');
+         var k = idT[1];
+         var pwp = parseFloat($('td#pwp-'+k).html());
+         var pad = parseFloat($(this).val());
+         var inter1= Number(pad*100);
+         var inter3= Number(inter1/pwp);
+         var rad = Number(100 - inter3);
+         $('input#rad-'+k).val(rad);
+          $(this).val(parseFloat($(this).val()));
+         
+     });
 });
