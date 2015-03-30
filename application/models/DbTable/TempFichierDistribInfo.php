aceb1631db4003dbc8c6aec7ea7878abb49622d6
FRHubyM <FRHubyM@FRD000448.smc-france>
2015-03-30 11:57:02 +0200

    creation validation xdistrib avec fichier

--- application/models/DbTable/TempFichierDistribInfo.php
+++ application/models/DbTable/TempFichierDistribInfo.php
@@ -13,7 +13,7 @@
 class Application_Model_DbTable_TempFichierDistribInfo extends Zend_Db_Table_Abstract{
     protected $_name='temp_fichier_distrib_info';
     
-    public function createInfo($numwp,$nom_distrib,$code_postal_distributeur,$ville_distributeur,$nom_contact,$nom_client,$numwp_client,$code_postal_client,$ville_client,$potentiel_client_final){
+    public function createInfo($numwp,$nom_distrib,$code_postal_distributeur,$ville_distributeur,$nom_contact,$nom_client,$numwp_client,$code_postal_client,$ville_client){
          $data = array(
            'numwp'                   =>  $numwp,
            'distrib'                 =>  $nom_distrib,
@@ -23,8 +23,7 @@ class Application_Model_DbTable_TempFichierDistribInfo extends Zend_Db_Table_Abs
            'nom_client_final'        =>  $nom_client,
            'numwp_client_final'      =>  $numwp_client,
            'codepostal_client_final' =>  $code_postal_client,
-           'ville_client_final'      =>  $ville_client,
-           'potentiel_client_final'  =>  $potentiel_client_final
+           'ville_client_final'      =>  $ville_client
         );
         $this->insert($data);
         return $this;   
