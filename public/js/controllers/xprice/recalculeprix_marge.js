842dd78e321042564201486fe6e06249121d7768
frhubym <frhubym@FRD000448.smc-france>
2015-04-09 13:17:47 +0200

    creation validation xdistrib avec fichier

--- public/js/controllers/xprice/recalculeprix_marge.js
+++ public/js/controllers/xprice/recalculeprix_marge.js
@@ -112,6 +112,7 @@ $(document).ready(function (){
           calculTotal();
         moyenne();
         moyenneMarge();
+        calculTotalcif();
     });
     
     $('#ouiFob').click(function(){
