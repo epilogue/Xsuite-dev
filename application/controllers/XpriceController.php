7da565c45bf94bdbb6a9eee649ea9b7f33d97310
frhubym <frhubym@FRD000448.smc-france>
2015-04-16 10:11:10 +0200

    creation validation xdistrib avec fichier

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1097,11 +1097,11 @@ if($user->id_fonction == 3){
             }elseif($margemin==false && $datas['mamo'] >10 && $datas['validation']="nonValide"){
                  $datas['validation']="nonValide";
             }
-          echo  '<pre>',var_export($datas['validation']),'</pre>';exit();
-//          echo '<pre>',var_export($margemin),'</pre>';
+          echo  '<pre>',var_export($datas['validation']),'</pre>';
+          echo '<pre>',var_export($margemin),'</pre>';
           $mamo=  substr($datas['mamo'], 0,-1);
-//          echo '<pre>', var_export($mamo),'</pre>';
-//          exit();
+          echo '<pre>', var_export($mamo),'</pre>';
+          exit();
             $nouvelle_validation = new Application_Model_DbTable_Validationsxprice();
             $nouv_validation = $nouvelle_validation->createValidation(
                     $nom_validation, $date_validation, $datas['validation'], $datas['commentaire_dbd'], $user->id_user, $datas['tracking']);
