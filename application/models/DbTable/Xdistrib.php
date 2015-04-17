2ffb264eba641e3ef6857f2fda41557c908ffad0
frhubym <frhubym@FRD000448.smc-france>
2015-04-17 14:11:44 +0200

    creation validation xdistrib avec fichier

--- application/models/DbTable/Xdistrib.php
+++ application/models/DbTable/Xdistrib.php
@@ -96,11 +96,10 @@ class Application_Model_DbTable_Xdistrib extends Zend_Db_Table_Abstract {
         }
     }
      public function searchforDBD(){
-        $sql="select demande_xdistrib.num_workplace_demande_xdistrib, demande_xdistrib.id_demande_xdistrib, demande_xdistrib.tracking_number_demande_xdistrib,clients.nom_client,demande_xdistrib.date_demande_xdistrib,validations_demande_xdistrib.etat_validation,validations_demande_xdistrib.nom_validation,users.nom_user  from demande_xdistrib"
-                . " join clients on clients.numwp_client = demande_xdistrib.numwp_client"
-                . " join users on users.id_user = demande_xdistrib.id_user"
-                . " join validations_demande_xdistrib  on validations_demande_xdistrib.id_demande_xdistrib = demande_xdistrib.id_demande_xdistrib "
-                ." order by demande_xdistrib.id_demande_xdistrib desc, validations_demande_xdistrib.date_validation asc";
+        $sql="SELECT demande_xdistrib.id_demande_xdistrib, `num_workplace_demande_xdistrib`,demande_xdistrib. tracking_number_demande_xdistrib,users.nom_user,  `date_demande_xdistrib`, demande_xdistrib. id_user, client_distrib.nom_client,validations_demande_xdistrib.etat_validation  FROM `demande_xdistrib` 
+join users on users.id_user=demande_xdistrib.id_user
+join client_distrib on client_distrib.numwp_client = demande_xdistrib.numwp_client
+join validations_demande_xdistrib on validations_demande_xdistrib.id_demande_xdistrib = demande_xdistrib.id_demande_xdistrib";
    $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
         if (!$rest) {
