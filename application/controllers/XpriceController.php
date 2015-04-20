753a4c4d2b87970241e4dbf18d17aafda7b53ced
frhubym <frhubym@FRD000448.smc-france>
2015-04-20 13:34:37 +0200

    creation validation xdistrib avec fichier

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1016,7 +1016,6 @@ if($user->id_fonction == 3){
         $infos_client = new Application_Model_DbTable_Clients();
         $info_client = $infos_client->getClientnumwp($info_demande_xprice['numwp_client']);
         $this->view->info_client = $info_client[0];
-        echo '<pre>',var_export('$info_client'),'</pre>';
         $noms_industrie = new Application_Model_DbTable_Industry();
         $nom_industrie = $noms_industrie->getIndustry($info_client[0]['id_industry']);
         $destIndustry = $info_client[0]['id_industry'];
@@ -1901,9 +1900,9 @@ if($mailServiceClients[0]['mail_service_client']=='regionNord'){
         $this->view->info_user = $info_user;
         $infos_client = new Application_Model_DbTable_Clients();
         $info_client = $infos_client->getClientnumwp($info_demande_xprice['numwp_client']);
-        $this->view->info_client = $info_client;
+        $this->view->info_client = $info_client[0];
         $noms_industrie = new Application_Model_DbTable_Industry();
-        $nom_industrie = $noms_industrie->getIndustry($info_client['id_industry']);
+        $nom_industrie = $noms_industrie->getIndustry($info_client[0]['id_industry']);
         $this->view->nom_industrie = $nom_industrie;
         $infos_demande_article_xprice = new Application_Model_DbTable_DemandeArticlexprices();
         $info_demande_article_xprice = $infos_demande_article_xprice->getDemandeArticlexprice($numwp);
@@ -2101,9 +2100,9 @@ if($mailServiceClients[0]['mail_service_client']=='regionNord'){
         $this->view->info_user = $info_user;
         $infos_client = new Application_Model_DbTable_Clients();
         $info_client = $infos_client->getClientnumwp($info_demande_xprice['numwp_client']);
-        $this->view->info_client = $info_client;
+        $this->view->info_client = $info_client[0];
         $noms_industrie = new Application_Model_DbTable_Industry();
-        $nom_industrie = $noms_industrie->getIndustry($info_client['id_industry']);
+        $nom_industrie = $noms_industrie->getIndustry($info_client[0]['id_industry']);
         $this->view->nom_industrie = $nom_industrie;
         $infos_validation = new Application_Model_DbTable_Validationsxprice();
         $info_validation = $infos_validation->getValidation($nom_validation, $info_demande_xprice['tracking_number_demande_xprice']);
