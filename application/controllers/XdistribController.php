8d56ec6bfbe71b77a44856955b86ef891937b0ee
frhubym <frhubym@FRD000448.smc-france>
2015-04-21 14:22:44 +0200

    creation validation xdistrib avec fichier

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -111,7 +111,7 @@ class XdistribController extends Zend_Controller_Action
              }
          $recapitulatif2=$plopr;
      }
-      if($user->id_fonction ==5|| $user->id_fonction == 13){
+      if($user->id_fonction ==5|| $user->id_fonction == 13 || $user->id_fonction == 29 || $user->id_fonction == 23 || $user->id_fonction == 32){
          $recapitulatif1 = new Application_Model_DbTable_Xdistrib;
          $recapitulatif2=$recapitulatif1->searchforDBD();
          $r = array();
