40d31c3aa12b3df26a6b90cbea6494ac6f618f77
frhubym <frhubym@FRD000448.smc-france>
2015-04-21 13:39:20 +0200

    creation validation xdistrib avec fichier

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -103,9 +103,9 @@ class XdistribController extends Zend_Controller_Action
                  }
              }
          }
-         unset($recapitulatif2);
+         unset($recapitulatif2); echo '<pre>', var_export($r),'</pre>'; 
          foreach($r as $value){
-         //echo '<pre>', var_export($value),'</pre>'; 
+         echo '<pre>', var_export($value),'</pre>'; 
              if(preg_match($tracking1,$value['tracking_number_demande_xdistrib'])==1 || preg_match($tracking2,$value['tracking_number_demande_xdistrib'] )==1 ) {
                  $plopr[] =$value; 
              }
