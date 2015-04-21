9bb1cb6ad268af790b582f0b0aec6dd9ad98c72d
frhubym <frhubym@FRD000448.smc-france>
2015-04-21 09:23:07 +0200

    creation validation xdistrib avec fichier

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -1342,7 +1342,7 @@ if($this->getRequest()->isPost()){
                 $idvalidhisto = new Application_Model_DbTable_Validationsxdistrib();
                 $lastidvalid = $idvalidhisto->getValidation($formData['nom_validation'], $formData['tracking']);
                 $newhistocomm = new Application_Model_DbTable_HistoriqueCommentaire();
-                $newhisto = $newhistocomm->createHistorique($formData['tracking'], $lastidvalid[0]['id_validation'], $info_user->id_user);
+                $newhisto = $newhistocomm->createHistorique($formData['tracking'], $lastidvalid[0]['id_validation'], $user->id_user);
                 $lastidhisto = new Application_Model_DbTable_HistoriqueCommentaire();
                 $lasthisto = $lastidhisto->getHistorique($formData['tracking'], $lastidvalid[0]['id_validation']);
 
