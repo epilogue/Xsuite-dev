22ddcaa84a2c4c58f64642dbfaa95552048091fb
frhubym <frhubym@FRD000448.smc-france>
2015-04-23 16:03:23 +0200

    creation validation xdistrib avec fichier

--- application/models/DbTable/Xdistrib.php
+++ application/models/DbTable/Xdistrib.php
@@ -109,7 +109,7 @@ join validations_demande_xdistrib on validations_demande_xdistrib.id_demande_xdi
         }
      }
      public function searchforDD($id){
-          $sql="select users.nom_user,demande_xdistrib.id_user,validations_demande_xdistrib.id_demande_xdistrib, demande_xdistrib.id_demande_xdistrib, demande_xdistrib.num_workplace_demande_xdistrib,demande_xdistrib.tracking_number_demande_xdistrib,client_distrib.nom_client,demande_xdistrib.date_demande_xdistrib from demande_xdistrib "
+          $sql="select validations_demande_xdistrib.etat_validation,users.nom_user,demande_xdistrib.id_user,validations_demande_xdistrib.id_demande_xdistrib, demande_xdistrib.id_demande_xdistrib, demande_xdistrib.num_workplace_demande_xdistrib,demande_xdistrib.tracking_number_demande_xdistrib,client_distrib.nom_client,demande_xdistrib.date_demande_xdistrib from demande_xdistrib "
                 . "join client_distrib on client_distrib.numwp_client = demande_xdistrib.numwp_client"
                   . " join validations_demande_xdistrib on validations_demande_xdistrib.id_demande_xdistrib = demande_xdistrib.id_demande_xdistrib "
                   . " join users on users.id_user=demande_xdistrib.id_user "
