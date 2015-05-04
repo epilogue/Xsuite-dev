cd47a5d0e8a1491e3ad2fe16d4b643ba2d06dd96
frhubym <frhubym@FRD000448.smc-france>
2015-05-04 14:00:55 +0200

    avenant

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -2700,7 +2700,7 @@ if($this->getRequest()->isPost()){
             $nom_validation = 'dirco';
             $this->nom_validation = $nom_validation;
             $datas = $this->getRequest()->getPost();
-            echo'<pre>',  var_export($datas),'</pre>';
+            echo'<pre>',  var_export($datas),'</pre>'; exit();
             $tracking=$datas['tracking'];
             $emailVars = Zend_Registry::get('emailVars');
             $prix_accordes = array_combine($datas['code_article'], $datas['prix_accorde_article']);
