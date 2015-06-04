9e9e6c306413878201a6df770ad862cff7975494
frhubym <frhubym@FRD000448.smc-france>
2015-06-04 14:51:07 +0200

    nomvalidation dans index

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -144,7 +144,7 @@ class XdistribController extends Zend_Controller_Action
          $demandes=new Application_Model_DbTable_Xdistrib();
          $demande= $demandes->tout();
          foreach($demande as $dem){
-             echo '<pre>',var_export($dem),'</pre>';
+             echo '<pre>',var_export($dem),'</pre>';exit();
          $recapitulatif2=$recapitulatif1->searchforDBD($dem);
          $r[]=$recapitulatif2;
          }
