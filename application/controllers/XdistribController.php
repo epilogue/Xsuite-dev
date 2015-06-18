72cebbf8779fd5c0e5de52ddcf3e10e7c9da4b2b
frhubym <frhubym@FRD000448.smc-france>
2015-06-18 15:54:08 +0200

    image dummy

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -87,7 +87,9 @@ class XdistribController extends Zend_Controller_Action
      if($user->id_fonction == 6){
          $recapitulatif1 = new Application_Model_DbTable_Xdistrib();
          $recapitulatif2 = $recapitulatif1->searchforDD($user->id_user);
-          echo '<pre>',var_export($recapitulatif2),'</pre>';
+          foreach ($recapitulatif2 as $k=>$value){
+              echo $k['date_validation'];
+          }
          $r = array();
          for ($index = 0; $index < count($recapitulatif2); $index++) {
              if(($index +1) > count($recapitulatif2)-1) {
