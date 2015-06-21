4c27e0a7de577cebb0f070f1fb45622b71291087
FRHubyM <FRHubyM@FRD000448.smc-france>
2015-06-21 15:27:33 +0200

    image dummy

--- application/models/DbTable/Xdistrib.php
+++ application/models/DbTable/Xdistrib.php
@@ -169,10 +169,11 @@ GROUP BY demande_xdistrib.`tracking_number_demande_xdistrib` order by demande_xd
       return $rest;
     }
     public function essaiTest($value){
-        $sql="select "
+        $sql="select validations_demande_xdistrib.nom_validation,"
+                . "validations_demande_xdistrib.etat_validation,"
+                . "validations_demande_xdistrib.date_validation,"
                 . "validations_demande_xdistrib.id_demande_xdistrib,"
                 . "users.nom_user,"
-                . "max(validations_demande_xdistrib.id),"
                 . "client_distrib.nom_client,"
                 . "demande_xdistrib.date_demande_xdistrib,"
                 . " demande_xdistrib.num_workplace_demande_xdistrib"
@@ -180,7 +181,11 @@ GROUP BY demande_xdistrib.`tracking_number_demande_xdistrib` order by demande_xd
                 ." join demande_xdistrib on demande_xdistrib.id_demande_xdistrib=validations_demande_xdistrib.id_demande_xdistrib"
                 ." join client_distrib on client_distrib.numwp_client = demande_xdistrib.numwp_client "
                 ." join users on users.id_user=demande_xdistrib.id_user"
-                . " where validations_demande_xdistrib.id_demande_xdistrib='$value' and demande_xdistrib.id_demande_xdistrib='$value'"
+                . " where validations_demande_xdistrib.id_demande_xdistrib='$value' and demande_xdistrib.id_demande_xdistrib='$value' "
+                . "and validations_demande_xdistrib.id = (select "
+                    . "max(validations_demande_xdistrib.id) "
+                    . "from validations_demande_xdistrib "
+                    . "where validations_demande_xdistrib.id_demande_xdistrib='$value')"
                 . " order by validations_demande_xdistrib.date_validation desc";
             $res= $this->getAdapter()->query($sql);
        $rest=$res->fetchAll();
