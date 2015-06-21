725e4808ef4e680c244bb575695746bd9550080b
FRHubyM <FRHubyM@FRD000448.smc-france>
2015-06-21 19:36:14 +0200

    image dummy

--- application/models/DbTable/Xdistrib.php
+++ application/models/DbTable/Xdistrib.php
@@ -229,6 +229,7 @@ JOIN validations_demande_xdistrib ON validations_demande_xdistrib.id_demande_xdi
                 . "join validations_demande_xdistrib on validations_demande_xdistrib.id_demande_xdistrib=demande_xdistrib.id_demande_xdistrib"
                 . "where validations_demande_xdistrib.id = (select max(validations_demande_xdistrib.id) from validations_demande_xdistrib)"
                 . " where demande_distrib.id_demande_xdistrib='$value'";
+        $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
         if (!$rest) {
             return null;
