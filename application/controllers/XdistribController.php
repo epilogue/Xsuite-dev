c6b6fd397eb8e415220cd7ee99e8f92d9b8fe7ec
frhubym <frhubym@FRD000448.smc-france>
2015-06-25 13:36:23 +0200

    image dummy

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -133,6 +133,7 @@ class XdistribController extends Zend_Controller_Action
       if($user->id_fonction ==5|| $user->id_fonction == 13 || $user->id_fonction == 29 || $user->id_fonction == 23 || $user->id_fonction == 32){
          $recapitulatif1 = new Application_Model_DbTable_Xdistrib;
          $plop1=$recapitulatif1->rechercheridDBD();
+            echo '<pre>',var_export($plop1) ,'</pre>';
          //$plop1=array_unique($plopi);
          foreach($plop1 as $value){
              $recapitulatif2[] =$recapitulatif1->rechercheDBD($value['id_demande_xdistrib']);
