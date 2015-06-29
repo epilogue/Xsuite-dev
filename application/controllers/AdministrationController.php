d8cc8fdc4ae317d14a416453ccb1c49a86ffe38a
frhubym <frhubym@FRD000448.smc-france>
2015-06-29 16:09:59 +0200

    menu administration

--- application/controllers/AdministrationController.php
+++ application/controllers/AdministrationController.php
@@ -22,9 +22,68 @@ class AdministrationController extends Zend_Controller_Action
     {
         // action body
     }
+    public function indexUserAction(){
+        
+    }
     public function createUserAction(){
     
     }
-
+    public function updateUserAction(){
+    
+    }
+    public function deleteUserAction(){
+    
+    }
+    public function indexFonctionAction(){
+        
+    }
+    public function createFonctionAction(){
+    
+    }
+    public function updateFonctionAction(){
+    
+    }
+    public function deleteFonctionAction(){
+    
+    }
+    public function indexZoneAction(){
+        
+    }
+    public function createZoneAction(){
+    
+    }
+    public function updateZoneAction(){
+    
+    }
+    public function deleteZoneAction(){
+    
+    }
+    public function indexIndustrieAction(){
+        
+    }
+    public function createIndustrieAction(){
+    
+    }
+    public function updateIndustrieAction(){
+    
+    }
+    public function deleteIndustrieAction(){
+    
+    }
+    public function indexHolonAction(){
+        
+    }
+    public function createHolonAction(){
+    
+    }
+    public function updateHolonAction(){
+    
+    }
+    public function deleteHolonAction(){
+    
+    }
+    public function indexRequeteAction(){
+        
+    }
 }
 
