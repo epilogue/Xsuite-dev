2a9eb49e307f5063876e20ef2bfdbf71ef3a84b8
frhubym <frhubym@FRD000448.smc-france>
2015-07-15 09:49:26 +0200

    debug xprice  prix fob et cif

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -2768,12 +2768,12 @@ if($mailServiceClients[0]['mail_service_client']=='regionNord'){
         var_dump($info_client);
         $this->view->info_client = $info_client;
         $noms_industrie = new Application_Model_DbTable_Industry();
-        $nom_industrie = $noms_industrie->getIndustry($info_client['id_industry']);
+        $nom_industrie = $noms_industrie->getIndustry($info_client[0]['id_industry']);
         $this->view->nom_industrie = $nom_industrie;
         $infos_demande_article_xprice = new Application_Model_DbTable_DemandeArticlexprices();
         $info_demande_article_xprice = $infos_demande_article_xprice->getDemandeArticlexprice($numwp);
         $this->view->info_demande_article_xprice = $info_demande_article_xprice;
-        $nomclients = trim($info_client['nom_client']);
+        $nomclients = trim($info_client[0]['nom_client']);
         
         
         if ($this->getRequest()->isPost()) {
