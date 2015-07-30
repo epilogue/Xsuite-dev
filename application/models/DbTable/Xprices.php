664f2a0667e91e42f1eac6feb0cf2986586df9a6
frhubym <frhubym@FRD000448.smc-france>
2015-07-30 16:24:05 +0200

    nettoyage

--- application/models/DbTable/Xprices.php
+++ application/models/DbTable/Xprices.php
@@ -141,7 +141,7 @@ public function searchAll($tracking_number){
             }
         } 
         public function searchForLeader($id_holon){
-            $sql = "select demande_xprices.num_workplace_demande_xprice, demande_xprices.id_demande_xprice, demande_xprices.tracking_number_demande_xprice,clients.nom_client,demande_xprices.date_demande_xprice,users.nom_user,validations_demande_xprices.etat_validation,validations_demande_xprices.nom_validation  from demande_xprices"
+            $sql = "select demande_xprices.num_workplace_demande_xprice, demande_xprices.id_demande_xprice, demande_xprices.tracking_number_demande_xprice,clients.nom_client,demande_xprices.date_demande_xprice,users.nom_user  from demande_xprices"
                     . " join clients on clients.numwp_client = demande_xprices.numwp_client"
                     . " join users on users.id_user = demande_xprices.id_user"
                      //. " join validations_demande_xprices  on validations_demande_xprices.id_demande_xprice = demande_xprices.id_demande_xprice "
