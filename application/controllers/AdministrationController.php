1cf2d5b1730036725b918b64ba7104699dbc46e8
frhubym <frhubym@FRD000448.smc-france>
2015-09-01 17:11:06 +0200

    update user

--- application/controllers/AdministrationController.php
+++ application/controllers/AdministrationController.php
@@ -72,13 +72,13 @@ class AdministrationController extends Zend_Controller_Action
      $utilisateur = $utilisateurs->getUser($id_user);
      $this->view->utilisateur = $utilisateur;
      $holons=new Application_Model_DbTable_Holons();
-     $holon=$holons->getHolon($this->utilisateur['id_holon']);
+     $holon=$holons->getHolon($utilisateur['id_holon']);
      $this->view->holon = $holon;
      $fonctions=new Application_Model_DbTable_Fonctions();
-     $fonction=$fonctions->getFonction($this->utilisateur['id_fonction']);
+     $fonction=$fonctions->getFonction($utilisateur['id_fonction']);
      $this->view->fonction = $fonction;
      $zones=new Application_Model_DbTable_Zones();
-     $zone=$zones->getZone($this->utilisateur['id_zone']);
+     $zone=$zones->getZone($utilisateur['id_zone']);
      $this->view->zone = $zone;
     }
     public function indexfonctionAction(){
