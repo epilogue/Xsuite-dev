b7efe3aab985f8cb3fc2973a2ffa031460cd394d
frhubym <frhubym@FRD000448.smc-france>
2015-09-08 10:50:09 +0200

    changement avenant brammer

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -3599,9 +3599,9 @@ if($this->getRequest()->isPost()){
         $dates= new Application_Model_DbTable_Validationsdemandexdistrib();
         $date= $dates->datefermeture($numwp);
         echo '<pre>', var_export($date),'</pre>'; 
-        $format='d/m/y';
+        $format='yy-mm-dd';
         
-        $datefinale1=  DateTime::createFromFormat($format, $date[0]['date_validation']);
+        $datefinale1=  DateTime::createFromFormat($format,$date[0]['date_validation']);
           echo '<pre>', var_export($datefinale1),'</pre>'; exit();
         $this->view->date=$datefinale1;
         $this->view->client_distrib=$client_info;
