bd7b99e19effa7e1d5dc417fe030d6274c195c4d
frhubym <frhubym@FRD000448.smc-france>
2015-11-13 10:44:05 +0100

    pb enregistrement de donnees

--- application/controllers/XpriceController.php
+++ application/controllers/XpriceController.php
@@ -1073,7 +1073,7 @@ if($user->id_fonction == 3 || $user->id_fonction==1){
             $nom_validation = 'dbd';
             $this->nom_validation = $nom_validation;
             $datas = $this->getRequest()->getPost();
-           // echo '<pre>',var_export($datas),'</pre>';exit();
+            echo '<pre>',var_export($datas),'</pre>';exit();
            $etatValide= $datas['validation'];
             $prix_accordes = array_combine($datas['code_article'], $datas['prix_accorde_article']);
             $remise_accordes = array_combine($datas['code_article'], $datas['remise_accorde_article']);
