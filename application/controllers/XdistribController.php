17e2d21ef3ebd5eff8f694195ca359864ee3610d
frhubym <frhubym@FRD000448.smc-france>
2016-08-09 10:36:30 +0200

    fichier  createnofile

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -293,6 +293,8 @@ class XdistribController extends Zend_Controller_Action
          var_dump($adresse);
          $codepostal = substr($adresse,0,5);
          var_dump($codepostal);
+         $ville = substring($adresse,4);
+         var_dump($ville);
 //         $clientDistrib = new Application_Model_DbTable_ClientDistrib();
 //         $client_distrib = $clientDistrib->createClientDistrib($numwp, $infos_client['OKCUNO'], $codepostal_client, $ville_client, $nom_industry, $id_industry, $nom_client, $client_pac);
     } 
