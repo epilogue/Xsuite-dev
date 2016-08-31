86105e35d9127a1d611c5c0b3e45135aea123a1a
frhubym <frhubym@FRD000448.smc-france>
2016-08-31 10:31:18 +0200

    debug xdistrib fob

--- application/models/DbTable/DemandeArticlexdistrib.php
+++ application/models/DbTable/DemandeArticlexdistrib.php
@@ -64,8 +64,8 @@ class Application_Model_DbTable_DemandeArticlexdistrib extends Zend_Db_Table_Abs
     public function updatecif($cifs, $code_article, $tracking_number) {
         
         $tracking_number = "$tracking_number";
-        $sql="update demande_article_xdistrib set prix_cif_demande_article=$cifs where code_article =$code_article And tracking_number_demande_xdistrib =  $tracking_number";
-//        var_dump($sql);
+        $sql="update demande_article_xdistrib set prix_cif_demande_article=$cifs where code_article =$code_article And tracking_number_demande_xdistrib =$tracking_number";
+        var_dump($sql);
         $res = $this->getAdapter()->query($sql);
         
         if (!$res) {
