514d873ef150b730b4053091408956e2569b0a56
frhubym <frhubym@FRD000448.smc-france>
2016-09-23 13:26:00 +0200

    createnofile

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -305,7 +305,7 @@ class XdistribController extends Zend_Controller_Action
                 $this->view->affiche_offre = $affiche_offre;
 //                echo '<pre>',var_export($this->view->affiche_offre),'</pre>';
             }
-        exit();
+       
 //           $this->view->affiche_offre=$affiche_offre;
            
 //            affiche_offre=$affiche_offres;
