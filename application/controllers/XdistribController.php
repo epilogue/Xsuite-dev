d2e98af7a106513fd9b9f33682b9efb32b2e3562
frhubym <frhubym@FRD000448.smc-france>
2016-09-23 11:35:42 +0200

    createnofile

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -413,6 +413,7 @@ class XdistribController extends Zend_Controller_Action
       $result = array_combine($formData['reference'],$formData['quantite']);
       $result2 =  array_combine( $formData['reference'],$formData['prix_tarif_dis']);
       $result3 = array_combine($formData['reference'],$formData['serie']);
+      $result4= array_combine($formData['reference'], $formData['prix_achat_client_final']);
       $redirector = $this->_helper->getHelper('Redirector');
         echo '<pre>',var_export($result),'</pre>';
         echo '<pre>',var_export($result2),'</pre>';
