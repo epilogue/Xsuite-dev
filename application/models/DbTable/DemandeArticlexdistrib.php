b92e5f172f855b2093f951f3b6f3856a553cacc8
frhubym <frhubym@FRD000448.smc-france>
2016-09-26 09:18:03 +0200

    createnofile

--- application/models/DbTable/DemandeArticlexdistrib.php
+++ application/models/DbTable/DemandeArticlexdistrib.php
@@ -28,12 +28,12 @@ class Application_Model_DbTable_DemandeArticlexdistrib extends Zend_Db_Table_Abs
         return $this;
     }
 public function createArticleDemandeNoFile($data){
-    $data[0]=  floatval($data[0]);
-    $data[1]=  floatval($data[1]);
-    $data[2]= floatval($data[2]);
-    $data[3]= floatval($data[3]);
-    $data[4]= intval($data[4]);
-       echo '<pre>',var_export($data, true),'</pre>';
+//    $data[0]=  floatval($data[0]);
+//    $data[1]=  floatval($data[1]);
+//    $data[2]= floatval($data[2]);
+//    $data[3]= floatval($data[3]);
+//    $data[4]= intval($data[4]);
+       echo '<pre>',var_export($data, true),'</pre>'; exit();
     $this->insert($data);
         return $this;
 }
