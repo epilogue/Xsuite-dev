494c32155ce5badabf40db77b49634bf9dabf185
frhubym <frhubym@FRD000448.smc-france>
2017-01-06 11:24:43 +0100

    createnofile

--- application/models/DbTable/DemandeArticlexdistrib.php
+++ application/models/DbTable/DemandeArticlexdistrib.php
@@ -102,14 +102,23 @@ public function createArticleDemandeNoFile($data){
      public function updateSerie($code_article,$tracking_number,$serie) {
 //        $code_article = "$code_article";
 //        $tracking_number = "$tracking_number";
-        $serie = "plop";
-        $plop4 = $this->getAdapter();
-        $datas = array('serie' => $serie);
-        $where = $plop4->quoteInto('code_article = ?', $code_article)
-                . $plop4->quoteInto(' And tracking_number_demande_xdistrib = ?', $tracking_number);
-        $plop5 = $this->update($datas, $where); echo $this->update($datas,$where); var_dump($datas);var_dump($where);var_dump($plop5);
-        return $plop5;
-       
+         $sql ="UPDATE `demande_article_xdistrib` SET `serie`={$serie}  WHERE `code_article`={$code_article} and `tracking_number_demande_xdistrib`={$tracking_number}";
+
+          $res = $this->getAdapter()->query($sql);
+        
+        if (!$res) {
+            return null;
+        } else {
+            return $res;
+        }
+//         $serie = "plop";
+//        $plop4 = $this->getAdapter();
+//        $datas = array('serie' => $serie);
+//        $where = $plop4->quoteInto('code_article = ?', $code_article)
+//                . $plop4->quoteInto(' And tracking_number_demande_xdistrib = ?', $tracking_number);
+//        $plop5 = $this->update($datas, $where); echo $this->update($datas,$where); var_dump($datas);var_dump($where);var_dump($plop5);
+//        return $plop5;
+//       
     }
 
     public function listtracking($tracking_number) {
