a2808ac669857119b38fedf9bfe4d7b2109f0b0e
frhubym <frhubym@FRD000448.smc-france>
2017-01-06 09:44:22 +0100

    createnofile

--- application/models/DbTable/DemandeArticlexdistrib.php
+++ application/models/DbTable/DemandeArticlexdistrib.php
@@ -109,7 +109,7 @@ public function createArticleDemandeNoFile($data){
                 . $plop4->quoteInto(' And tracking_number_demande_xdistrib = ?', $tracking_number);
         $plop5 = $this->update($datas, $where);
         return $plop5;
-        var_dump($plop5);exit();
+        var_dump($datas);var_dump($where);exit();
     }
 
     public function listtracking($tracking_number) {
