15b8cd03be96b750b6d71dcf7bcb82d782e93541
frhubym <frhubym@FRD000448.smc-france>
2017-01-19 16:40:24 +0100

    createnofile

--- application/controllers/XdistribController.php
+++ application/controllers/XdistribController.php
@@ -713,7 +713,7 @@ if($this->getRequest()->isPost()){
             $infos_offre = odbc_exec($this->odbc_conn, $pirate);
             $infos_offres = odbc_fetch_object($infos_offre);
             $this->view->infos_offres = $infos_offres;
-//            echo '<pre>',  var_export($infos_offres),'</pre>'; (exit);
+            echo '<pre>',  var_export($infos_offres),'</pre>'; (exit);
             $nomdeb = trim($infos_offres->OBCHID);
 //             echo '<pre>',  var_export($infos_offres),'</pre>';
 //             var_dump($infos_offres->OBCHID);
