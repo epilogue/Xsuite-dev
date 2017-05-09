4269478fe395d6ff3e0952fd33bc02c2a07b9d7a
frhubym <frhubym@FRD000448.smc-france>
2017-05-09 11:15:23 +0200

    createnofile

--- application/controllers/AdministrationController.php
+++ application/controllers/AdministrationController.php
@@ -26,7 +26,7 @@ $host="{webmail.smc-france.fr:143/imap}"; // voir http://fr.php.net/imap_open
 $login="frhubym"; //imap login
 $password="plop08"; //imap password
 $savedirpath="./Bureau/testconfig" ; // attachement will save in same directory where scripts run othrwise give abs path
-$jk=new MailAttachmentManager($host, $login, $password, $savedirpath); // Creating instance of class####
+//$jk=new MailAttachmentManager($host, $login, $password, $savedirpath); // Creating instance of class####
     }
 
     public function indexAction()
