9c47681bbe931184e4ea8d8016c08588a7f1e3a8
frhubym <frhubym@FRD000448.smc-france>
2017-05-09 14:03:57 +0200

    createnofile

--- public/config.php
+++ public/config.php
@@ -3,5 +3,5 @@ $login="frhubym"; //imap login
 $password="plop08"; //imap password
 $savedirpath="/home/mag/Bureau/testconfig" ; // attachement will save in same directory where scripts run othrwise give abs path
 $mbName = 'INBOX'; // nom de la mailBox
-$host="{webmail.smc-france.fr:143/imap{$mbName}"; // voir http://fr.php.net/imap_open
+$host="{webmail.smc-france.fr:143/imap}$mbName"; // voir http://fr.php.net/imap_open
 
