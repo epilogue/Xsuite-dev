5828abe67b8383a47b836f2261d08150da6ff7c9
frhubym <frhubym@FRD000448.smc-france>
2017-05-09 11:26:01 +0200

    createnofile

--- application/controllers/AdministrationController.php
+++ application/controllers/AdministrationController.php
@@ -21,7 +21,7 @@ class AdministrationController extends Zend_Controller_Action
     
     public function testmailAction()
     {
-       //require_once 'public/ClassphpImap.php';
+       require_once 'library/Xsuite/Controller/Plugin/ClassphpImap.php';
 $host="{webmail.smc-france.fr:143/imap}"; // voir http://fr.php.net/imap_open
 $login="frhubym"; //imap login
 $password="plop08"; //imap password
