4bbf39c5cd718bc2fb8ca941a47a8f415560ef3d
frhubym <frhubym@FRD000448.smc-france>
2017-06-12 13:24:08 +0200

    xprev index template en fonction des niveaaux et creation des array selon le niveau

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -20,9 +20,19 @@ class XprevController extends Zend_Controller_Action
     public function indexAction()
     {
         $user = $this->_auth->getStorage()->read();
-        $fn1 =array(1,2,4,6,43,44,46,26,27,28,229,30);
+        /*fonction niveau0*/
+        $fn0 = array(4);
+        /* fonction niveau1*/
+        $fn1 =array(1,2,6,43,44,46,26,27,28,29,30,34,35,36,37,40);
+        /*fonction niveau2*/
+        $fn2 = array(3,10,41,45,42);
+        /*fonction niveau3*/
+        $fn3 = array(32,23,50,39);
         $this->view->fonction = $user->id_fonction;
+        $this->view->fn1 = $fn0;
         $this->view->fn1 = $fn1;
+        $this->view->fn1 = $fn2;
+        $this->view->fn1 = $fn3;
     }
 
 
