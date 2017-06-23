6b5e0925a48f32b4984e9a3b194cf03696a09185
frhubym <frhubym@FRD000448.smc-france>
2017-06-23 16:20:31 +0200

    modification  creation.phtml

--- public/js/controllers/xprev/datepicker.js
+++ public/js/controllers/xprev/datepicker.js
@@ -3,10 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ *  Type: Date or Number or String
+Default: null
+The minimum selectable date. When set to null, there is no minimum.
+Multiple types supported:
 
+    Date: A date object containing the minimum date.
+    Number: A number of days from today. For example 2 represents two days from today and -1 represents yesterday.
+    String: A string in the format defined by the dateFormat option,
+     or a relative date. Relative dates must contain value and period pairs;
+      valid periods are "y" for years, "m" for months, "w" for weeks, and "d" for days. 
+      For example, "+1m +7d" represents one month and seven days from today.
+
+Code examples:
+
+Initialize the datepicker with the minDate option specified:
+1
+2
+3
+	
+
+$( ".selector" ).datepicker({
+  minDate: new Date(2007, 1 - 1, 1)
+});
+ */
 
 $(document).ready(function(){
     $( function() {
-    $( "#datepicker" ).datepicker();
+    $( "#datepicker" ).datepicker({
+        minDate : new Date("+1m")
+    });
   } );
 });
