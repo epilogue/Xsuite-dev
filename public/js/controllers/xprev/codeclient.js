c4dfc07f6d4e5dc128f7174feb5473573b8935ea
frhubym <frhubym@FRD000448.smc-france>
2017-07-01 09:30:34 +0200

    ajout des models pour le module xprev

---
+++ public/js/controllers/xprev/codeclient.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    
+    $('#num_client').change(function(e)
+    {
+        e.preventDefault();
+        console.log( $('#nom_client option[value="'+$(this).val()+'"]'));
+        console.log( $('#num_client option[value="'+$(this).val()+'"]'));
+         $('#nom_client option[value="'+$(this).val()+'"]');
+    });
+});
+
