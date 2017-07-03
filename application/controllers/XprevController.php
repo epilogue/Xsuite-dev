ce1435f097727e7be3d77f0aabbeb224a824eca0
frhubym <frhubym@FRD000448.smc-france>
2017-07-03 09:29:10 +0200

    ajout des models pour le module xprev

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -26,6 +26,11 @@ class XprevController extends Zend_Controller_Action
         $listecodeuser = $codeuser->getAllcodeuser($num_client);
         $this->view->listecodeuser = $listecodeuser;
     }
+    public function liaisonmoisAction(){
+        $this->_helper->layout->disableLayout();
+        $num_mois = $this->getRequest()->getParam($date_debut);
+        var_dump($num_mois);
+    }
     public function indexAction()
     {
         $user = $this->_auth->getStorage()->read();
