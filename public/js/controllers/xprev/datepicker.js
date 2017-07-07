3e3767972b92a2c65bb8354855c3435263c3a4f8
frhubym <frhubym@FRD000448.smc-france>
2017-07-07 14:14:28 +0200

    fichier / bouton submit

--- public/js/controllers/xprev/datepicker.js
+++ public/js/controllers/xprev/datepicker.js
@@ -28,11 +28,20 @@ $( ".selector" ).datepicker({
   minDate: new Date(2007, 1 - 1, 1)
 });
  */
+var articleId = 0;
 
+function newArticleLine() {
+    var baseHtml = $('#addRefTPL').html();
+    var toHtml = baseHtml.replace(/__id__/g, articleId);
+    $("#table_mois").append(toHtml);
+    var usedAid = articleId;
+    articleId++;
+    return usedAid;
+}
 $(document).ready(function(){
      $("#date_debut").MonthPicker({
        lang:'fr',
-        MonthFormat: 'mm-yy',
+       MonthFormat: 'mm-yy',
        MinMonth:1,
        MaxMonth:'+1y',
        OnAfterChooseMonth: function() { 
@@ -44,6 +53,60 @@ $(document).ready(function(){
           function(data){
               $('#table_mois').html(data);
           },'html');
+          $("#ajout_article").click(function(e){
+         e.preventDefault();
+         console.log('plop');
+        var id = newArticleLine();
+        console.log(id);
+                 //name="refart[__id__][reference]"
+        $('input[name="refart['+id+'][reference]"]').change(function(e)
+        {
+            console.log('tagada');
+              $.get ('/xprev/verifreference/reference/'+$(this).val(),
+              {},
+              function(data){
+                  if(!$.trim(data)){
+                  alert("la reference rentree n'existepas dans movex");
+              }
+                  $('input[name="refart['+id+'][code_article]"]').val(data);
+              },'html');
+              
+        });
+    //});
+        $('.resetbuton').unbind('click');
+        $('.resetbuton').click(function(e){
+             e.preventDefault();
+            //alert($(this).data('id'));
+             $('input[name="refart['+$(this).data('id')+'][reference]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][code_article]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m1]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m2]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m3]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m4]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m5]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m6]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m7]"]').val(""); 
+             $('input[name="refart['+$(this).data('id')+'][m8]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m9]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m10]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m11]"]').val("");
+             $('input[name="refart['+$(this).data('id')+'][m12]"]').val(""); 
+        });
+    });
+    var id = newArticleLine();
+    $('input[name="refart['+id+'][reference]"]').change(function(e)
+        {
+              $.get ('/xprev/verifreference/reference/'+$(this).val(),
+              {},
+              function(data){
+                   if(!$.trim(data)){
+                  alert("la reference rentree n'existepas dans movex"); }
+                  $('input[name="refart['+id+'][code_article]"]').val(data);
+              },'html');
+             
+        });
+    $('select[name="refart[0][reference]"]').attr('required',true);
+    $('input[name="refart[0][code_article]"]').attr('required',true);
     }
     });
 });
