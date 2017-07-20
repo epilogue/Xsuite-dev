07ff2fd9ace7a89f6bbd1cb163987d8216425bce
frhubym <frhubym@FRD000448.smc-france>
2017-07-20 09:01:33 +0200

    xprev envoi mail validation N+1

--- application/models/DbTable/HierarchieXprev.php
+++ application/models/DbTable/HierarchieXprev.php
@@ -5,7 +5,7 @@ class Application_Model_DbTable_HierarchieXprev extends Zend_Db_Table_Abstract {
     protected $_name = 'hierarchie_xprev';
 
     public function getHierarchie($holon,$fonction){
-        $sql= "select users.email_user,hierarchie.id_user from hierarchie_xprev join users on users.id_user = hierarchie_xprev.id_user where hierarchie_xprev.id_holon =$holon and hierarchie_xprev.id_fonction = $fonction";
+        $sql= "select users.email_user,hierarchie_xprev.id_user from hierarchie_xprev join users on users.id_user = hierarchie_xprev.id_user where hierarchie_xprev.id_holon ={$holon} and hierarchie_xprev.id_fonction ={$fonction}";
         $res= $this->getAdapter()->query($sql);
       $rest = $res->fetchObject();
      if (!$rest) {
