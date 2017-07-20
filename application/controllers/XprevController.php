822a3de5bba711366c9477425a4042026dd17029
frhubym <frhubym@FRD000448.smc-france>
2017-07-20 09:39:39 +0200

    xprev envoi mail validation N+1

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -240,8 +240,8 @@ class XprevController extends Zend_Controller_Action
              $emailVars = Zend_Registry::get('emailVars');
              /* creation des parametre du mail*/
              $params=array();
-             $params['destinataire']=$destinataire->email_user;
-             
+            // $params['destinataire']=$destinataire->email_user;
+             $params['destinataire']="mhuby@smc-france.fr";
              $params['url'] = "http://{$_SERVER['SERVER_NAME']}/xprev/validn1/tracking/{$trackingnumber}";
              $params['corpsMail']="Bonjour,\n"
                                 . "\n"
@@ -255,6 +255,7 @@ class XprevController extends Zend_Controller_Action
                                 . "Xsuite";
              $params['sujet']="validation Xprev $trackingnumber ";
               echo '<pre>',  var_export($params),'</pre>';
+               $this->sendEmail($params);
         }
     }
 }
