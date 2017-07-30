fb5f295b3e7c875e9c5d099b0d01b789634fbb90
frhubym <frhubym@FRD000448.smc-france>
2017-07-30 08:15:32 +0200

    xprev accueil liste des demandes

--- application/models/DbTable/DemandeXprev.php
+++ application/models/DbTable/DemandeXprev.php
@@ -110,7 +110,7 @@ public function getAllcodeuser($code_client) {
                 . " join holon on holon.id_holon=users.id_holon "
                 . " join client_user_xprev on client_user_xprev.id_client_user_xprev=demande_xprev.id_client_user_xprev "
                 . " join designation_validation_xprev  on designation_validation_xprev.id_designation_validation_xprev = demande_xprev.id_statut_xprev "
-                . " where holon.nom_holon like '{$holon_court}%'";
+                . " where holons.nom_holon like '{$holon_court}%'";
                 $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
         if (!$rest) {
