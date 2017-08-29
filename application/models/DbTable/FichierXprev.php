365d789a98a76da7472ad95c4d0eb03a73edab10
frhubym <frhubym@FRD000448.smc-france>
2017-08-29 16:33:25 +0200

    xprev requete extraction information demande article xprev

--- application/models/DbTable/FichierXprev.php
+++ application/models/DbTable/FichierXprev.php
@@ -12,7 +12,7 @@ class Application_Model_DbTable_FichierXprev extends Zend_Db_Table_Abstract
         return $this;
     }
    public function getfichier($tracking){
-       $sql="select from fichier_xprev where fichier_xprev.tracking_xprev like '{$tracking}'";
+       $sql="select * from fichier_xprev where fichier_xprev.tracking_xprev like '{$tracking}'";
         $res = $this->getAdapter()->query($sql);
         $rest=$res->fetchAll();
         if (!$rest) {
