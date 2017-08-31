cb2c9067ab484dddcff94683eba52a1d2bb1bdff
frhubym <frhubym@FRD000448.smc-france>
2017-08-31 11:19:49 +0200

    xprev requete extraction information demande article xprev

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -382,7 +382,23 @@ class XprevController extends Zend_Controller_Action
         $this->view->infoUser = $infoUser;
     }
     public function validregAction(){
-        
+        $user = $this->_auth->getStorage()->read();
+        /*information concernant la personne connectée*/
+        $User = new Application_Model_DbTable_Users();
+        $infoUser = $User->getUser($user->id_user);
+        $tracking = $this->getRequest()->getParam('tracking', null);
+        var_dump($tracking);
+        $Prev = new Application_Model_DbTable_DemandeXprev();
+        $infoPrev = $Prev->getprev($tracking);
+        $fichier = new Application_Model_DbTable_FichierXprev();
+        $infoFichier = $fichier->getfichier($tracking);
+        $ArticlePrev = new Application_Model_DbTable_DemandeArticleXprev();
+        $infoArticle = $ArticlePrev->getarticleprev($tracking);
+        echo '<pre>',  var_export($infoUser),'</pre>';
+        $this->view->infoPrev = $infoPrev[0];
+        $this->view->infoArticle = $infoArticle;
+        $this->view->infoFichier = $infoFichier;
+        $this->view->infoUser = $infoUser;
     }
     public function validfobAction(){
         
