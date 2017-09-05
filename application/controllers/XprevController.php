0f69e255f20b91a75d75eda6cf860f2bf64509bf
frhubym <frhubym@FRD000448.smc-france>
2017-09-05 14:23:31 +0200

    validlog/validdop/traitement

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -501,7 +501,11 @@ class XprevController extends Zend_Controller_Action
         $this->view->infoUser = $infoUser;
         if($this->getRequest()->isPost()){
             $formData =  $this->getRequest()->getPost();
-            echo '<pre>',  var_export($formData),'</pre>'; exit();
+            echo '<pre>',  var_export($formData),'</pre>';
+            
+            $revient = array_combine($formData['code_article'], $formData['prix_revient']);
+            echo '<pre>',  var_export($revient),'</pre>';
+                    exit();
             /*mettre à jour la demande xprev 
              * au niveau du nom de la validation
              * commentaire validation
