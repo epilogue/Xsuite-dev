7ec10a58274eb145a9f2494637cc7a883658a22f
frhubym <frhubym@FRD000448.smc-france>
2017-09-12 10:20:28 +0200

    listeUser/validLog

--- public/js/controllers/xprev/calculTotal.js
+++ public/js/controllers/xprev/calculTotal.js
@@ -1,9 +1,15 @@
 $(document).ready(function (){
     $('.changepr').change(function(){
+        var valeurTotal = 0;
         var totalM= parseInt($(this).data('msums'));
         var amount= parseFloat($(this).val());
         var finalAmount = totalM * amount;
         
         $('#'+$(this).data('targetid')).val(finalAmount);
+        
+        $('.valeurTotale').each(function(i,e){
+            valeurTotal+=parseFloat($(e).val());
+        });
+        $('#spanTotal').html(valeurTotal);
     });
 });
