5ac933140751bf4f639b3179c5649176a98cbaea
frhubym <frhubym@FRD000448.smc-france>
2017-09-15 14:48:40 +0200

    recherche

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -1187,25 +1187,25 @@ class XprevController extends Zend_Controller_Action
         if($this->getRequest()->isPost()){
             $formData =  $this->getRequest()->getPost();
             echo '<pre>',  var_export($formData),'</pre>';
-            if(!array_key_exists(['nom_client'], $formData)){
+            if(!array_key_exists($formData['nom_client'], $formData)){
                 $formData['nom_client']=null;
             }
-            if(!array_key_exists(['nom_commercial'], $formData)){
+            if(!array_key_exists($formData['nom_commercial'], $formData)){
                 $formData['nom_commercial']=null;
             }
-            if(!array_key_exists(['nom_emetteur'], $formData)){
+            if(!array_key_exists($formData['nom_emetteur'], $formData)){
                 $formData['nom_emetteur']=null;
             }
-            if(!array_key_exists(['nom_statut'], $formData)){
+            if(!array_key_exists($formData['nom_statut'], $formData)){
                 $formData['nom_statut']=null;
             }
-            if(!array_key_exists(['reference'], $formData)){
+            if(!array_key_exists($formData['reference'], $formData)){
                 $formData['reference']=null;
             }
-            if(!array_key_exists(['tracking'], $formData)){
+            if(!array_key_exists($formData['tracking'], $formData)){
                 $formData['tracking']=null;
             }
-            if(!array_key_exists(['datecreate'], $formData)){
+            if(!array_key_exists($formData['datecreate'], $formData)){
                 $formData['datecreate']=null;
             }
         }else{
