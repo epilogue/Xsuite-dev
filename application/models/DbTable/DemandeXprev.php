d1897354ffd9e233ea5eeaa165311522920be546
frhubym <frhubym@FRD000448.smc-france>
2017-09-21 09:57:09 +0200

    recherche

--- application/models/DbTable/DemandeXprev.php
+++ application/models/DbTable/DemandeXprev.php
@@ -37,16 +37,16 @@ class Application_Model_DbTable_DemandeXprev extends Zend_Db_Table_Abstract {
                     $sqlsuite2.= " or demande_article_xprev.reference_article='{$formdata['reference']}'";    
                 }
                 
-            }
+            }if(!empty($formdata['nom_client'])){
+                    $sqlsuite.= " and client_xprev.code_client_user_xprev= {$formdata['nom_client']}";
+                    if(!empty($formdata['reference'])){
+                    $sqlsuite2.= " or demande_article_xprev.reference_article='{$formdata['reference']}'";    
+                    }
+                }
             
             
         }  else {
-            if(!empty($formdata['nom_client'])){
-                    $sqlsuite.= " and client_xprev.code_client_user_xprev= {$formdata['nom_client']}";
-                    if(!empty($formdata['tracking'])){
-                        $sqlsuite2.= " or demande_xprev.tracking='{$formdata['tracking']}'";
-                     }
-                }
+            
         }
         
         
