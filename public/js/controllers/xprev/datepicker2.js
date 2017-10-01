20ec6083c2d406493bb99db7f390b797614f47e9
frhubym <frhubym@FRD000448.smc-france>
2017-10-01 09:45:16 +0200

    modification pour test final

---
+++ public/js/controllers/xprev/datepicker2.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    $("#datepicker1").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+    $("#datepicker2").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+    $("#datepicker3").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+    $("#datepicker4").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+    $("#datepicker5").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+    $("#datepicker6").datepicker({
+        dateFormat:"dd-mm-yy"
+    });
+});
+
