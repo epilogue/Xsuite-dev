1733b33780b18c29a7447561e2a6084180c810ea
frhubym <frhubym@FRD000448.smc-france>
2017-10-02 17:12:09 +0200

    modification pour test final

--- public/js/controllers/xprev/datepicker.js
+++ public/js/controllers/xprev/datepicker.js
@@ -97,7 +97,7 @@ $(document).ready(function(){
              e.preventDefault();
              //$('tr[name ="ligne['+$(this).data('id')+']"]').remove();
              //$("tr").eq($(this).data('id')).remove();
-              $('#table_mois tr:eq('+$(this).data('id')+')').remove();
+              $('#table_mois tr:eq('+$(this).data('id')+'+1)').remove();
              // $('#table_mois thead').html(data);
 //          },'html');
             //alert($(this).data('id'));
