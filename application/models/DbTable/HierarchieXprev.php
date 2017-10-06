03f74e1172a14302399b03cdfe274565f201e6d5
frhubym <frhubym@FRD000448.smc-france>
2017-10-06 14:28:27 +0200

    administration gestion hierarchie xprev

--- application/models/DbTable/HierarchieXprev.php
+++ application/models/DbTable/HierarchieXprev.php
@@ -17,8 +17,8 @@ class Application_Model_DbTable_HierarchieXprev extends Zend_Db_Table_Abstract {
     public function affichehierarchienord(){
         $sql = "select users.nom_user,fonctions.nom_fonction, holons.nom_holon from hierarchie_xprev "
                 . "left join users on users.id_user = hierarchie_xprev.id_user "
-                . " left join holons on holons.id_holon = user.id_holon "
-                . " left join fonctions on fonctions.id_fonction= user.id_fonction "
+                . " left join holons on holons.id_holon = users.id_holon "
+                . " left join fonctions on fonctions.id_fonction= users.id_fonction "
                 . " where holons.nom_holon like 'IN%' ";
         var_dump($sql);exit();
         $res = $this->getAdapter()->query($sql);
