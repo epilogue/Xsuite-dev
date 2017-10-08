2321df167f94cb3a17101470049288927445f3b2
frhubym <frhubym@FRD000448.smc-france>
2017-10-08 12:42:46 +0200

    administration gestion hierarchie xprev

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -1600,7 +1600,7 @@ var_dump($d1);
 $d2=new DateTime($listXprev[$keytab]['fin_de_validite']);
 var_dump($d2);
 $interval =$d1 ->diff($d2);
-var_dump($interval);
+var_dump($interval['y']);
 $nbremois = $interval->format('%d');
 var_dump($nbremois);
 //while ($monthdeb !=$monthfin and $yeardeb != $yearfin){
