4f8827169c88295634cfaf504cdb0465ffbd548d
frhubym <frhubym@FRD000448.smc-france>
2017-10-08 18:23:11 +0200

    administration gestion hierarchie xprev

--- application/models/DbTable/Extract.php
+++ application/models/DbTable/Extract.php
@@ -9,7 +9,7 @@ class Application_Model_DbTable_Extract extends Zend_Db_Table_Abstract {
         return $this;
    }
    
-   public function upquantite($tracking,$code,$reference,$quantite){
+   public function upquantite($tracking,$code,$quantite){
        $sql = "update extract_xprev set quantite = $quantite ,set code_article= '{$code}' where tracking like '{$tracking}'";
        $res = $this->getAdapter()->query($sql);
        
