a8ca80d904e809691e3f80324af35bf08701d817
frhubym <frhubym@FRD000448.smc-france>
2017-10-08 15:18:18 +0200

    administration gestion hierarchie xprev

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -221,7 +221,9 @@ class XprevController extends Zend_Controller_Action
             }
             // creation des dates pour l'insertions dans la table extract_xprev
             foreach($tab as $table){
-               echo '<pre>',  var_export($table),'</pre>'; exit();
+                $table .=array('day'=>1);
+               echo '<pre>',  var_export($table),'</pre>';
+               exit();
             }
             /** mise au format des date*/
             $datedebut1= '01-'.$formData['date_debut'];
