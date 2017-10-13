4a0708f8acfc0c04e402b4d9fae9010ae5d41e8e
frhubym <frhubym@FRD000448.smc-france>
2017-10-13 06:37:43 +0200

    administration gestion hierarchie xprev

--- application/controllers/RoutineController.php
+++ application/controllers/RoutineController.php
@@ -25,11 +25,19 @@ class RoutineController extends Zend_Controller_Action {
                 ->send();
     }
     public function routineAction() {
+        $dateinterval= new DateInterval("P3M");
+        $nDate = new DateTime();
+        $nDate->add($dateinterval);
+        //var_dump($nDate->format('Y-m-d'));
+        $datemailing = $nDate->format('Y-m-d');
+        $Mailing = new Application_Model_DbTable_DemandeXprev();
+        $listeMailing = $Mailing->mailinglist($datemailing);
+       // echo '<pre>',  var_export($listeMailing),'</pre>';
        $emailVars = Zend_Registry::get('emailVars');
              /* creation des parametre du mail*/
-//         foreach($listeMailing as $listeMailing1){
+         foreach($listeMailing as $listeMailing1){
              $params=array();
-//             $params['destinataireMail']=$listeMailing1['email_user'];
+             //$params['destinataireMail']=$listeMailing1['email_user'];
              $params['destinataireMail']="mhuby@smc-france.fr";
              $params['corpsMail']="Bonjour,\n"
                                 . "\n"
@@ -45,8 +53,33 @@ class RoutineController extends Zend_Controller_Action {
              $params['sujet']="fin de validité de la prévision dans 3 mois pour la prévision {$listeMailing1['tracking']} pour {$listeMailing1['nom_client']}";
               //echo '<pre>',  var_export($params),'</pre>';
                $this->sendEmail($params);
-//         }
+         }
     }
-
+public function routinebisAction(){
+        $date = new DateTime();
+        $newdate = $date->format('Y-m-d');
+         $Mailing = new Application_Model_DbTable_DemandeXprev();
+        $listeMailingob = $Mailing->mailinglistob($newdate);
+         $emailVars = Zend_Registry::get('emailVars');
+             /* creation des parametre du mail*/
+         foreach($listeMailingob as $listeMailingob1){
+             $params=array();
+             $params['destinataireMail']=$listeMailingob1['email_user'];
+//             $params['destinataireMail']="mhuby@smc-france.fr";
+             $params['corpsMail']="Bonjour,\n"
+                                . "\n"
+                                . "pour information la prévision {$listeMailingob1['tracking']} pour le client {$listeMailingob1['nom_client']}est arrivée en fin de validité.\n"
+                                ." Merci de faire le point avec client et de nous faire éventuellement une Xprev\n "
+                                . " avec un commentaire sur les raisons de la demande de mise en stock et les engagements clients.\n"
+                                . "\n\n"
+                                . "Cordialement,\n"
+                                . "\n"
+                                . "--\n"
+                                . "Le service Logistique";
+             $params['sujet']="fin de validité de la prévision  {$listeMailingob1['tracking']} pour {$listeMailingob1['nom_client']}";
+              //echo '<pre>',  var_export($params),'</pre>';
+             $this->sendEmail($params);
+         }
+    }
 }
 
