ce3a6b1b02979c9fb8c45aa7831fe2b19fc48264
frhubym <frhubym@FRD000448.smc-france>
2017-10-13 13:50:12 +0200

    administration gestion hierarchie xprev

--- public/js/controllers/xprev/datepicker.js
+++ public/js/controllers/xprev/datepicker.js
@@ -169,7 +169,8 @@ $(document).ready(function(){
               $.get ('/xprev/verifreference/reference/'+$(this).val(),
               {},
               function(data){
-                   if(!$.trim(data)){
+                   if(!$(this).trim(data)){
+                       alert();
                   alert("la reference rentree n'existe pas dans movex"); }
                   $('input[name="refart['+id+'][code_article]"]').val(data);
               },'html');
