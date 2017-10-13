998a8100ba938c514c89f9f1349eba0a267e4a20
frhubym <frhubym@FRD000448.smc-france>
2017-10-13 16:42:54 +0200

    administration gestion hierarchie xprev

--- application/controllers/XprevController.php
+++ application/controllers/XprevController.php
@@ -107,7 +107,7 @@ class XprevController extends Zend_Controller_Action
         $xprev = new Application_Model_DbTable_DemandeXprev();
         $Holon = new Application_Model_DbTable_Holons();
         $infoHolon =$Holon->getHolon($user->id_holon); 
-        "select MITMAS.MMITNO from EIT.MVXCDTA.MITMAS MITMAS where  trim(MITMAS.MMITDS) ='CHAPE G8X32 TOURILLON PM8X32' and  MITMAS.MMCONO='100'";
+        $requete1 ="select MITMAS.MMITNO from EIT.MVXCDTA.MITMAS MITMAS where  trim(MITMAS.MMITDS) ='CHAPE G8X32 TOURILLON PM8X32' and  MITMAS.MMCONO='100'";
         var_dump($requete1);
         $results1 = odbc_exec($this->odbc_conn2, $requete1);
         $res =  odbc_fetch_array($results1);
